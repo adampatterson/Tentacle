88a357aa864308bfe17b6a168e6735e547f2e057
Adam Patterson <hello@adampatterson.ca>
2011-09-27 16:51:04 -0600

    Cleared from index.

--- application/controller/dev.php
+++ application/controller/dev.php
@@ -13,11 +13,6 @@ class dev_controller {
 		//echo '<h3>Dev Land</h3>';
 		//echo route::controller().'_'. route::method() .'<br />';
 		
-		echo '<h3>Git pull test</h3>';
-		
-		$output = shell_exec('git pull');
-		echo "<pre>$output</pre>";
-
 	}
 	
 	public function pull()
