8a68ffda5b6b9f09a7e02ca1faaffd072041b0ef
Adam Patterson <hello@adampatterson.ca>
2011-10-25 16:34:06 -0600

    Updated version.php (converted variables to globals

--- application/config/deployment/version.php
+++ application/config/deployment/version.php
@@ -1,47 +1,46 @@
 <?php
 /**
- * The Tentacle version
- *
- * @global string $tentacle_version
- */
-$tentacle_version = '1.0';
+* The Tentacle version
+*
+* @global string $tentacle_version
+*/
+define('TENTACLE_VERSION', '1.0');
+
+/**
+* The Dingo PHP Framework version
+*
+* @global string $dingo_version
+*/
+define('MVC_VERSION', DINGO_VERSION);
 
 
 /**
- * The Dingo PHP Framework version
- *
- * @global string $dingo_version
- */
-$dingo_version = '1.0';
+* Holds the Tentacle DB revision, increments when changes are made to the Tentacle DB schema.
+*
+* @global int $tentacle_db_version
+*/
+define('TENTACLE_DB_VERSION', '1.0');
 
 
 /**
- * Holds the Tentacle DB revision, increments when changes are made to the Tentacle DB schema.
- *
- * @global int $tentacle_db_version
- */
-$tentacle_db_version = '1.0';
+* Holds the markItUp! version
+*
+* @global string $editor_version
+*/
+define('EDITOR_VERSION', '1.0');
 
 
 /**
- * Holds the markItUp! version
- *
- * @global string $editor_version
- */
-$editor_version = '1.0';
+* Holds the required PHP version
+*
+* @global string $required_php_version
+*/
+define('REQUIRED_PHP_VERSION', '5.2');
 
 
 /**
- * Holds the required PHP version
- *
- * @global string $required_php_version
- */
-$required_php_version = '5.2';
-
-
-/**
- * Holds the required MySQL version
- *
- * @global string $required_mysql_version
- */
-$required_mysql_version = '4.1.2';
+* Holds the required MySQL version
+*
+* @global string $required_mysql_version
+*/
+define('REQUIRED_MYSQL_VERSION', '4.1.2');
