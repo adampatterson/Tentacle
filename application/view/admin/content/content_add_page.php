1eb6847a7a3b2b52ff8ea9c58619f6d4b8d1f99e
Adam Patterson <hello@adampatterson.ca>
2011-11-18 14:06:46 -0700

    Udpates

--- application/view/admin/content/content_add_page.php
+++ application/view/admin/content/content_add_page.php
@@ -105,16 +105,16 @@
 										mode: "text/html",
 										onCursorActivity: function() {
 										    editor.setLineClass(hlLine, null);
-										    hlLine = editor.setLineClass(editor.getCursor().line, "activeline");
-										  },
-								        onKeyEvent: function(cm, e) {
-								          // Hook into ctrl-space
-								          if (e.keyCode == 32 && (e.ctrlKey || e.metaKey) && !e.altKey) {
-								            e.stop();
-								            return CodeMirror.simpleHint(cm, CodeMirror.javascriptHint);
-								          }
-								        }
-								      });
+											hlLine = editor.setLineClass(editor.getCursor().line, "activeline");
+										},
+										onKeyEvent: function(cm, e) {
+											// Hook into ctrl-space
+											if (e.keyCode == 32 && (e.ctrlKey || e.metaKey) && !e.altKey) {
+												e.stop();
+												return CodeMirror.simpleHint(cm, CodeMirror.javascriptHint);
+											}
+										}
+									});
 								var hlLine = editor.setLineClass(0, "activeline");
 								    </script>
 							<? endif; ?>
