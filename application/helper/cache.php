66d1156418834328cbfb61fac3e1c0c4e2cf3eff
Adam Patterson <hello@adampatterson.ca>
2011-12-22 22:57:38 -0700

    Cache blueprint

---
+++ application/helper/cache.php
@@ -0,0 +1,49 @@
+<?
+/**
+* Object Caching
+*/
+class cache
+{
+	
+	function __construct()
+	{
+		# code...
+	}
+	
+	
+	function add( $key, $data, $group, $expire )
+	{
+		
+	}
+	
+	
+	function set( $key, $data, $group, $expire )
+	{
+		
+	}
+	
+	
+	function get( $key, $group )
+	{
+		
+	}
+	
+	
+	function delete( $id, $group ) 
+	{
+		
+	}
+	
+	
+	function replace( $key, $data, $group, $expire )
+	{
+		
+	}
+	
+	
+	function flush() 
+	{
+		
+	}
+}
+
