005192f1b9c89484638f2444b36cf0c8c7b432d9
Adam Patterson <hello@adampatterson.ca>
2012-01-17 15:19:17 -0700

    Cleared out minified code chunk
    Minification will come at a later date. Some JS does not work when Minified.

--- tentacle/admin/bundles/application.php
+++ tentacle/admin/bundles/application.php
@@ -6,28 +6,7 @@ _/_/_/_/_/                      _/                          _/
  _/    _/        _/    _/    _/      _/    _/  _/        _/  _/          
 _/      _/_/_/  _/    _/      _/_/    _/_/_/    _/_/_/  _/    _/_/_/     
 ======================================================================-->                                                                 
-<? //if (CONFIGURATION == 'deployment'): ?>
-<!--
-	<link type="text/css" rel="stylesheet" href="<?=MINIFY; ?>b=http/dev.tcms.me/tentacle/admin/css&f=bootstrap-1.4.0.min.css,general.css,admin.css" />
 
-
-	<script type="text/javascript" src="<?=TENTACLE_JS; ?>jquery.min.js"></script>
-	<script type="text/javascript" src="<?=TENTACLE_JS; ?>modernizr-2.0.6.min.js"></script>
-
-	<script type="text/javascript" src="<?=TENTACLE_JS; ?>jquery.notice.js"></script>
-	<script type="text/javascript" src="<?=TENTACLE_JS; ?>jquery.inputtags.js"></script>
-
-	<script type="text/javascript" src="<?=TENTACLE_JS; ?>bootstrap-tabs.min.js"></script>
-	<script type="text/javascript" src="<?=TENTACLE_JS; ?>bootstrap-alerts.min.js"></script>
-	<script type="text/javascript" src="<?=TENTACLE_JS; ?>bootstrap-dropdown.min.js"></script>
-	<script type="text/javascript" src="<?=TENTACLE_JS; ?>bootstrap-scrollspy.min.js"></script>
-	<script type="text/javascript" src="<?=TENTACLE_JS; ?>bootstrap-modal.min.js"></script>
-	<script type="text/javascript" src="<?=TENTACLE_JS; ?>bootstrap-twipsy.min.js"></script>
-	<script type="text/javascript" src="<?=TENTACLE_JS; ?>bootstrap-popover.min.js"></script>
-
-	<script type="text/javascript" src="<?=MINIFY; ?>b=http/dev.tcms.me/tentacle/admin/js&f=spin.min.js,sisyphus.min.js,jquery.validate.js,application.js"></script>
--->
-<? //else: ?>
 	<link type="text/css" rel="stylesheet" href="<?=TENTACLE_CSS; ?>bootstrap-1.4.0.min.css">
 	<link type="text/css" rel="stylesheet" href="<?=TENTACLE_CSS; ?>general.css">
 	<link type="text/css" rel="stylesheet" href="<?=TENTACLE_CSS; ?>admin.css">
@@ -53,7 +32,6 @@ _/      _/_/_/  _/    _/      _/_/    _/_/_/    _/_/_/  _/    _/_/_/
 	
 	<script type="text/javascript" src="<?=TENTACLE_JS; ?>application.js"></script>
 	
-<? //endif;?>
 	<!-- <script type="text/javascript" charset="utf-8">
 		$(document).ready(function(){
 			$('#username').keyup(username_check);
