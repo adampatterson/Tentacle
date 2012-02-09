1f0c43b181d47022eddc5905c9d442ff0a58e108
Adam Patterson <hello@adampatterson.ca>
2012-02-09 10:40:52 -0700

    Shell script for curl install
    WIP  curl get.tcms.me | sh
    
    There is an error when TAR tries to extract the file

---
+++ get.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# curl get.tcms.me | sh
+
+# Set up
+
+      echo "*** Installing Tentacle CMS..."
+
+# Download Tentacle and unpack it.
+# Move the files front the Tar to the root web folder www.domain.com/
+
+      curl -s https://github.com/adampatterson/Tentacle/tarball/master | tar xzf -
+
+# All done!
