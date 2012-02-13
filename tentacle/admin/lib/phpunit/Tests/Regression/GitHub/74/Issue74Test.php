0e43317365842cdbbccbf9bb4a911893e76e8c2b
Adam Patterson <hello@adampatterson.ca>
2012-02-13 14:36:00 -0700

    House keeping

--- tentacle/admin/lib/phpunit/Tests/Regression/GitHub/74/Issue74Test.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Issue74Test extends PHPUnit_Framework_TestCase
-{
-    public function testCreateAndThrowNewExceptionInProcessIsolation()
-    {
-        require_once dirname(__FILE__) . '/NewException.php';
-        throw new NewException('Testing GH-74');
-    }
-}
