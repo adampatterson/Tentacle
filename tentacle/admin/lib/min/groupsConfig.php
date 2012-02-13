88c38acab7c9ab49d46346acd0404f1b43360c4e
Adam Patterson <hello@adampatterson.ca>
2012-02-13 15:09:58 -0700

    house keeping

---
+++ tentacle/admin/lib/min/groupsConfig.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Groups configuration for default Minify implementation
+ * @package Minify
+ */
+
+/** 
+ * You may wish to use the Minify URI Builder app to suggest
+ * changes. http://yourdomain/min/builder/
+ *
+ * See http://code.google.com/p/minify/wiki/CustomSource for other ideas
+ **/
+
+return array(
+    // 'js' => array('//js/file1.js', '//js/file2.js'),
+    // 'css' => array('//css/file1.css', '//css/file2.css'),
+ 	
+	//'keyName' => array('//http/dev.tcms.me/tentacle/admin/css/bootstrap-1.4.0.min.css', '//http/dev.tcms.me/tentacle/admin/css/general.css', '//http/dev.tcms.me/tentacle/admin/css/admin.css'),
+);
