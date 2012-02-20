a8b8aeffc676bc9ca49d173d315a9062f6b9fc98
Adam Patterson <hello@adampatterson.ca>
2012-02-20 12:36:16 -0700

    marketing cleanup

--- tentacle/themes/marketing/index.php
+++ tentacle/themes/marketing/index.php
@@ -24,7 +24,7 @@ if(!defined('SCAFFOLD')):
 		<a href="<?=BASE_URL;?>"><img src="<?=BASE_URL;?>tentacle/admin/images/tentacle_logo_large.png" width="258" height="63" alt="Tentacle" /></a>
 	</div>
 	<div id="login-content-message">
-		<p><strong>Tentacle is an OpenSource Content Management System, and it's free to use!</strong></p>
+		<p><strong>Tentacle is an OpenSource Content Management System, and it's easy to use!</strong></p>
 		<p>Follow us on Twitter <a href="https://twitter.com/#!/TentacleCMS" class='' target="_blank">@TentacleCMS</a>, read the <a href='http://www.tentaclecms.com/blog/'>Blog</a>, and consider joining the mailing list.</p>
 	</div>
 
@@ -52,6 +52,7 @@ if(!defined('SCAFFOLD')):
 		<?php endif;?>
 	</form>
 </div>
+<a href="https://github.com/adampatterson/Tentacle/tree/beta-wip"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/abad93f42020b733148435e2cd92ce15c542d320/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub"></a>
   <!-- #login-content -->
 <? load_part('footer'); 
 endif;
--- tentacle/themes/marketing/part-footer.php
+++ tentacle/themes/marketing/part-footer.php
@@ -17,42 +17,5 @@
 
 </script>
 <!-- End Google Analytics -->
-
-<!-- begin olark code -->
-<script type='text/javascript'>/*{literal}<![CDATA[*/window.olark||(function(i){var e=window,h=document,a=e.location.protocol=="https:"?"https:":"http:",g=i.name,b="load";(function(){e[g]=function(){(c.s=c.s||[]).push(arguments)};var c=e[g]._={},f=i.methods.length; while(f--){(function(j){e[g][j]=function(){e[g]("call",j,arguments)}})(i.methods[f])} c.l=i.loader;c.i=arguments.callee;c.f=setTimeout(function(){if(c.f){(new Image).src=a+"//"+c.l.replace(".js",".png")+"&"+escape(e.location.href)}c.f=null},20000);c.p={0:+new Date};c.P=function(j){c.p[j]=new Date-c.p[0]};function d(){c.P(b);e[g](b)}e.addEventListener?e.addEventListener(b,d,false):e.attachEvent("on"+b,d); (function(){function l(j){j="head";return["<",j,"></",j,"><",z,' onl'+'oad="var d=',B,";d.getElementsByTagName('head')[0].",y,"(d.",A,"('script')).",u,"='",a,"//",c.l,"'",'"',"></",z,">"].join("")}var z="body",s=h[z];if(!s){return setTimeout(arguments.callee,100)}c.P(1);var y="appendChild",A="createElement",u="src",r=h[A]("div"),G=r[y](h[A](g)),D=h[A]("iframe"),B="document",C="domain",q;r.style.display="none";s.insertBefore(r,s.firstChild).id=g;D.frameBorder="0";D.id=g+"-loader";if(/MSIE[ ]+6/.test(navigator.userAgent)){D.src="javascript:false"} D.allowTransparency="true";G[y](D);try{D.contentWindow[B].open()}catch(F){i[C]=h[C];q="javascript:var d="+B+".open();d.domain='"+h.domain+"';";D[u]=q+"void(0);"}try{var H=D.contentWindow[B];H.write(l());H.close()}catch(E){D[u]=q+'d.write("'+l().replace(/"/g,String.fromCharCode(92)+'"')+'");d.close();'}c.P(2)})()})()})({loader:(function(a){return "static.olark.com/jsclient/loader0.js?ts="+(a?a[1]:(+new Date))})(document.cookie.match(/olarkld=([0-9]+)/)),name:"olark",methods:["configure","extend","declare","identify"]});
-/* custom configuration goes here (www.olark.com/documentation) */
-olark.identify('6479-579-10-4369');/*]]>{/literal}*/</script>
-<!-- end olark code -->
-
-<!-- Spring Metrics -->
-<script type='text/javascript'>
- var _springMetq = _springMetq || [];
- _springMetq.push(['id', '3b0e0e175e']);
- (
-  function(){
-   var s = document.createElement('script');
-   s.type = 'text/javascript';
-   s.async = true;
-   s.src = ('https:' == document.location.protocol ? 'https://d3rmnwi2tssrfx.cloudfront.net/a.js' : 'http://static.springmetrics.com/a.js');
-   var x = document.getElementsByTagName('script')[0];
-   x.parentNode.insertBefore(s, x);
-  }
- )();
-</script>
-<!-- end Spring Metrics -->
-
-<!-- Kiss Metricks -->
-<script type="text/javascript">
-  var _kmq = _kmq || [];
-  function _kms(u){
-    setTimeout(function(){
-      var s = document.createElement('script'); var f = document.getElementsByTagName('script')[0]; s.type = 'text/javascript'; s.async = true;
-      s.src = u; f.parentNode.insertBefore(s, f);
-    }, 1);
-  }
-  _kms('//i.kissmetrics.com/i.js');_kms('//doug1izaerwt3.cloudfront.net/e8ae2abedae3cb6b72d71819d1aebf6bb0e65d36.1.js');
-</script>
-<!-- end Kiss Metricks -->
-
 </body>
 </html>
