7a3d8cad9ccd2ace33b8822abca0fb5ee7e93526
Adam Patterson <hello@adampatterson.ca>
2012-02-22 16:57:52 -0700

    close #71 Added snippet helper for use in the template code.

---
+++ application/helper/snippet.php
@@ -0,0 +1,15 @@
+<?
+function get_snippet( $atts ) {
+	$snippet = load::model ( 'snippet' );
+	$snippet_single = $snippet->get_slug( $atts );
+	
+	return $snippet_single->content;
+	}
+	
+	
+function snippet( $atts ) {
+	$snippet = load::model ( 'snippet' );
+	$snippet_single = $snippet->get_slug( $atts['slug'] );
+	
+	return $snippet_single->content;
+	}
