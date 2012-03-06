330f84c7af4ba0bdcbdb3868b8531d1d39f0061b
Adam Patterson <hello@adampatterson.ca>
2012-03-06 15:12:49 -0700

    Get and Is Functions
    get_author()
    
    is_installed()

---
+++ application/helper/get.php
@@ -0,0 +1,50 @@
+<?
+/*
+ * These Get functions are meant to the used more so on the admin side.
+ */
+
+	function get_db_version ()
+	{
+		return get_option( 'db_version' );
+	}
+
+		
+	/**
+	 * Create a really nice url like http://www.example.com/controller/action/params#anchor
+	 *
+	 * you can put as many params as you want,
+	 * if a params start with # it is considered to be an Anchor
+	 *
+	 * get_url('controller/action/param1/param2') // I always use this method
+	 * get_url('controller', 'action', 'param1', 'param2');
+	 *
+	 * @param string conrtoller, action, param and/or #anchor
+	 * @return string
+	 */
+	function get_url() 
+	{
+	    $params = func_get_args();
+	    if (count($params) === 1) return BASE_URL . $params[0];
+	    
+	    $url = '';
+	    foreach ($params as $param) {
+	        if (strlen($param)) {
+	            $url .= $param{0} == '#' ? $param: '/'. $param;
+	        }
+	    }
+	    return BASE_URL . preg_replace('/^\/(.*)$/', '$1', $url);
+	}
+	
+	
+	/**
+	 * Get the request method used to send this page
+	 *
+	 * @return string possible value: GET, POST or AJAX
+	 */
+	function get_request_method() 
+	{
+	    if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') return 'AJAX';
+	    else if ( ! empty($_POST)) return 'POST';
+	    else return 'GET';
+	}
+	
---
+++ application/helper/is.php
@@ -0,0 +1,36 @@
+<?
+/*
+ * These functions should return TRUE of FALSE
+ * They should be used to logic in templates and the admin area.
+ */
+
+	function is_blog_installed() {
+		if ( get_option( 'is_blog_installed' ) ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	function is_rtl() {}
+	
+	function is_front_page() {}
+	function is_home() {}
+	function is_date() {}
+	function is_search() {}
+	function is_paged() {}
+	function is_page() {}
+	function is_page_template() {}
+	function is_attachment() {}
+	function is_404() {}
+	function is_error() {}
+	function is_single() {}
+	function is_archive() {}
+	function is_post_type_archive() {}
+	function is_author() {}
+	function is_user_logged_in() {}
+	function is_category() {}
+	function is_tag() {}
+	function is_tax() {}
+	function is_admin_bar_showing() {}
+
