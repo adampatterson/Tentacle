5079970497829c6147801d9bcb8ae2eccd7f2067
Adam Patterson <hello@adampatterson.ca>
2012-03-08 10:42:46 -0700

    Dev Tests

--- application/controller/dev.php
+++ application/controller/dev.php
@@ -213,20 +213,10 @@ class dev_controller {
 		echo 'Token: ' . $_SESSION['ga_auth_token'];
 	}
 	
-	
-	
-	
+
 	public function sortable ()
-	{	
-		$page = load::model( 'page' );
-		$pages = $page->get( );
-		
-		$page_hiarchy = $page->get_page_children( 0, $pages );
-		
-		$user = load::model('user'); 
-		$options = load::model ( 'settings' );
-		
-		load::view ('admin/sortable', array( 'pages'=>$page_hiarchy, 'user'=>$user ) );
+	{			
+		load::view ( 'admin/sortable' );
 	}
 	
 	
@@ -899,14 +889,13 @@ class dev_controller {
 				'notes' => 'This is a note'
 				));
 		
-		load::helper ('array');
-
-		$object = array_to_object ( $array );
+		
+		$object = (object)$array;
 		echo '<h2>Array to Object</h2><pre>';
 			print_r($object);
 		echo '</pre>';
 		
-		$array = object_to_array ( $object );
+		$array = (array)$object;
 		echo '<h2>Array to Object</h2><pre>';
 			print_r($array);
 		echo '</pre>';
@@ -980,6 +969,31 @@ class dev_controller {
 		echo '<h2>get_gravatar()</h2>';
 		
 		echo get_gravatar('adamapatterson@gmail.com');
+		
+		?>
+			<script type="text/javascript" src="<?=TENTACLE_JS; ?>jquery.min.js"></script>
+			<script type="text/javascript" src="<?=TENTACLE_JS; ?>md5.js"></script>
+		<script type="text/javascript" charset="utf-8">
+			$(document).ready(function(){
+
+			var email = $("#email"),
+			avatar = $("#avatar");
+
+			email.blur(function() {
+			avatar.css("background-image","url(http://www.gravatar.com/avatar/" + MD5(email.val()) + ")");
+
+			});
+
+			});
+			
+		</script>
+		<div id="avatar"><img src="http://papermashup.com/demos/gravatar-image/thumb.png" width="80" height="80"/></div>
+
+		<form class="gravatar">
+		<input type="text" onFocus="if(this.value=='Email Address') this.value='';" onBlur="if(this.value=='') this.value='Email Address';" value="Email Address" name="s" id="email" />
+		</form>
+		
+	<?
 	}
 	
 	/**
@@ -1369,4 +1383,282 @@ class dev_controller {
 		<?
 	}
 
+	public function upload_image ()
+	{
+		$post_id = '123';
+
+		$targetFolder = STORAGE_DIR.'images/'; // Relative to the root
+		
+		?>
+		<!DOCTYPE HTML>
+		<html lang="en">
+		<head>
+		<meta charset="utf-8">
+		<title>jQuery File Upload Demo</title>
+		<meta name="description" content="File Upload widget with multiple file selection, drag&amp;drop support, progress bar and preview images for jQuery. Supports cross-domain, chunked and resumable file uploads. Works with any server-side platform (Google App Engine, PHP, Python, Ruby on Rails, Java, etc.) that supports standard HTML form file uploads.">
+		<!-- Bootstrap CSS Toolkit styles -->
+		<link rel="stylesheet" href="http://blueimp.github.com/cdn/css/bootstrap.min.css">
+		<!-- Bootstrap styles for responsive website layout, supporting different screen sizes -->
+		<link rel="stylesheet" href="http://blueimp.github.com/cdn/css/bootstrap-responsive.min.css">
+		<!-- Bootstrap CSS fixes for IE6 -->
+		<!--[if lt IE 7]><link rel="stylesheet" href="http://blueimp.github.com/cdn/css/bootstrap-ie6.min.css"><![endif]-->
+		<!-- Bootstrap Image Gallery styles -->
+		<link rel="stylesheet" href="http://blueimp.github.com/Bootstrap-Image-Gallery/css/bootstrap-image-gallery.min.css">
+		<!-- CSS to style the file input field as button and adjust the Bootstrap progress bars -->
+		<link rel="stylesheet" href="<?= TENTACLE_CSS ?>/jquery.fileupload-ui.css">
+		<!-- Shim to make HTML5 elements usable in older Internet Explorer versions -->
+		<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+		</head>
+		<body>
+		<div class="container">
+		    <div class="page-header">
+		        <h1>File Upload</h1>
+		    </div>
+		    <br>
+		    <!-- The file upload form used as target for the file upload widget -->
+		    <form id="fileupload" action="<?= TENTACLE_JS ?>jQuery-File-Upload/server/php/" method="POST" enctype="multipart/form-data">
+		        <!-- The fileupload-buttonbar contains buttons to add/delete files and start/cancel the upload -->
+		        <div class="row fileupload-buttonbar">
+		            <div class="span7">
+		                <!-- The fileinput-button span is used to style the file input field as button -->
+		                <span class="btn btn-success fileinput-button">
+		                    <span><i class="icon-plus icon-white"></i> Add files...</span>
+		                    <input type="file" name="files[]" multiple>
+		                </span>
+		                <button type="submit" class="btn btn-primary start">
+		                    <i class="icon-upload icon-white"></i> Start upload
+		                </button>
+		                <button type="reset" class="btn btn-warning cancel">
+		                    <i class="icon-ban-circle icon-white"></i> Cancel upload
+		                </button>
+		                <button type="button" class="btn btn-danger delete">
+		                    <i class="icon-trash icon-white"></i> Delete
+		                </button>
+		                <input type="checkbox" class="toggle">
+		            </div>
+		            <div class="span5">
+		                <!-- The global progress bar -->
+		                <div class="progress progress-success progress-striped active fade">
+		                    <div class="bar" style="width:0%;"></div>
+		                </div>
+		            </div>
+		        </div>
+		        <!-- The loading indicator is shown during image processing -->
+		        <div class="fileupload-loading"></div>
+		        <br>
+		        <!-- The table listing the files available for upload/download -->
+		        <table class="table table-striped"><tbody class="files" data-toggle="modal-gallery" data-target="#modal-gallery"></tbody></table>
+		    </form>
+		    <br>
+		</div>
+		<!-- modal-gallery is the modal dialog used for the image gallery -->
+		<div id="modal-gallery" class="modal modal-gallery hide fade">
+		    <div class="modal-header">
+		        <a class="close" data-dismiss="modal">&times;</a>
+		        <h3 class="modal-title"></h3>
+		    </div>
+		    <div class="modal-body"><div class="modal-image"></div></div>
+		    <div class="modal-footer">
+		        <a class="btn btn-primary modal-next">Next <i class="icon-arrow-right icon-white"></i></a>
+		        <a class="btn btn-info modal-prev"><i class="icon-arrow-left icon-white"></i> Previous</a>
+		        <a class="btn btn-success modal-play modal-slideshow" data-slideshow="5000"><i class="icon-play icon-white"></i> Slideshow</a>
+		        <a class="btn modal-download" target="_blank"><i class="icon-download"></i> Download</a>
+		    </div>
+		</div>
+		<!-- The template to display files available for upload -->
+		<script id="template-upload" type="text/x-tmpl">
+		{% for (var i=0, file; file=o.files[i]; i++) { %}
+		    <tr class="template-upload fade">
+		        <td class="preview"><span class="fade"></span></td>
+		        <td class="name">{%=file.name%}</td>
+		        <td class="size">{%=o.formatFileSize(file.size)%}</td>
+		        {% if (file.error) { %}
+		            <td class="error" colspan="2"><span class="label label-important">{%=locale.fileupload.error%}</span> {%=locale.fileupload.errors[file.error] || file.error%}</td>
+		        {% } else if (o.files.valid && !i) { %}
+		            <td>
+		                <div class="progress progress-success progress-striped active"><div class="bar" style="width:0%;"></div></div>
+		            </td>
+		            <td class="start">{% if (!o.options.autoUpload) { %}
+		                <button class="btn btn-primary">
+		                    <i class="icon-upload icon-white"></i> {%=locale.fileupload.start%}
+		                </button>
+		            {% } %}</td>
+		        {% } else { %}
+		            <td colspan="2"></td>
+		        {% } %}
+		        <td class="cancel">{% if (!i) { %}
+		            <button class="btn btn-warning">
+		                <i class="icon-ban-circle icon-white"></i> {%=locale.fileupload.cancel%}
+		            </button>
+		        {% } %}</td>
+		    </tr>
+		{% } %}
+		</script>
+		<!-- The template to display files available for download -->
+		<script id="template-download" type="text/x-tmpl">
+		{% for (var i=0, file; file=o.files[i]; i++) { %}
+		    <tr class="template-download fade">
+		        {% if (file.error) { %}
+		            <td></td>
+		            <td class="name">{%=file.name%}</td>
+		            <td class="size">{%=o.formatFileSize(file.size)%}</td>
+		            <td class="error" colspan="2"><span class="label label-important">{%=locale.fileupload.error%}</span> {%=locale.fileupload.errors[file.error] || file.error%}</td>
+		        {% } else { %}
+		            <td class="preview">{% if (file.thumbnail_url) { %}
+		                <a href="{%=file.url%}" title="{%=file.name%}" rel="gallery" download="{%=file.name%}"><img src="{%=file.thumbnail_url%}"></a>
+		            {% } %}</td>
+		            <td class="name">
+		                <a href="{%=file.url%}" title="{%=file.name%}" rel="{%=file.thumbnail_url&&'gallery'%}" download="{%=file.name%}">{%=file.name%}</a>
+		            </td>
+		            <td class="size">{%=o.formatFileSize(file.size)%}</td>
+		            <td colspan="2"></td>
+		        {% } %}
+		        <td class="delete">
+		            <button class="btn btn-danger" data-type="{%=file.delete_type%}" data-url="{%=file.delete_url%}">
+		                <i class="icon-trash icon-white"></i> {%=locale.fileupload.destroy%}
+		            </button>
+		            <input type="checkbox" name="delete" value="1">
+		        </td>
+		    </tr>
+		{% } %}
+		</script>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
+		<script src="<?= TENTACLE_JS ?>jQuery-File-Upload/js/vendor/jquery.ui.widget.js"></script>
+		<!-- The Templates plugin is included to render the upload/download listings -->
+		<script src="http://blueimp.github.com/JavaScript-Templates/tmpl.min.js"></script>
+		<!-- The Load Image plugin is included for the preview images and image resizing functionality -->
+		<script src="http://blueimp.github.com/JavaScript-Load-Image/load-image.min.js"></script>
+		<!-- The Canvas to Blob plugin is included for image resizing functionality -->
+		<script src="http://blueimp.github.com/JavaScript-Canvas-to-Blob/canvas-to-blob.min.js"></script>
+		<!-- Bootstrap JS and Bootstrap Image Gallery are not required, but included for the demo -->
+		<script src="http://blueimp.github.com/cdn/js/bootstrap.min.js"></script>
+		<script src="http://blueimp.github.com/Bootstrap-Image-Gallery/js/bootstrap-image-gallery.min.js"></script>
+		<!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
+		<script src="<?= TENTACLE_JS ?>jQuery-File-Upload/js/jquery.iframe-transport.js"></script>
+		<!-- The basic File Upload plugin -->
+		<script src="<?= TENTACLE_JS ?>jQuery-File-Upload/js/jquery.fileupload.js"></script>
+		<!-- The File Upload image processing plugin -->
+		<script src="<?= TENTACLE_JS ?>jQuery-File-Upload/js/jquery.fileupload-ip.js"></script>
+		<!-- The File Upload user interface plugin -->
+		<script src="<?= TENTACLE_JS ?>jQuery-File-Upload/js/jquery.fileupload-ui.js"></script>
+		<!-- The localization script -->
+		<script src="<?= TENTACLE_JS ?>jQuery-File-Upload/js/locale.js"></script>
+		<!-- The main application script -->
+		<script src="<?= TENTACLE_JS ?>jQuery-File-Upload/js/main.js"></script>
+		<!-- The XDomainRequest Transport is included for cross-domain file deletion for IE8+ -->
+		<!--[if gte IE 8]><script src="<?= TENTACLE_JS ?>jQuery-File-Upload/js/cors/jquery.xdr-transport.js"></script><![endif]-->
+		</body> 
+		</html>
+		<?
+    }
+
+
+    public function text () {
+        ?>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+        <script type="text/javascript">
+			$(document).ready( function() {
+				$('#ClickWordList li').click(function() { 
+					$("#txtMessage").insertAtCaret($(this).html());
+					return false
+				});
+			});
+			
+			var $frame = $('<iframe style="width:200px; height:100px;">');
+			        $('body').html( $frame );
+			        setTimeout( function() {
+			                var doc = $frame[0].contentWindow.document;
+			                var $body = $('body',doc);
+			                $body.html('<h1>Test</h1>');
+			        }, 1 );
+			
+			$.fn.insertAtCaret = function (myValue) {
+				return this.each(function(){
+					//IE support
+					if (document.selection) {
+						this.focus();
+						sel = document.selection.createRange();
+						sel.text = myValue;
+						this.focus();
+					}
+					//MOZILLA / NETSCAPE support
+					else if (this.selectionStart || this.selectionStart == '0') {
+						var startPos = this.selectionStart;
+						var endPos = this.selectionEnd;
+						var scrollTop = this.scrollTop;
+						this.value = this.value.substring(0, startPos)+ myValue+ this.value.substring(endPos,this.value.length);
+						this.focus();
+						this.selectionStart = startPos + myValue.length;
+						this.selectionEnd = startPos + myValue.length;
+						this.scrollTop = scrollTop;
+					} else {
+						this.value += myValue;
+						this.focus();
+					}
+				});
+			};
+			</script>
+
+			<textarea name="txtMessage" id="txtMessage" class="txtDropTarget ui-droppable" cols="80" rows="15"></textarea>		
+
+			<legend>Click to insert:</legend>
+			<ul id="ClickWordList">
+				<li><a href="http://www.google.ca">Link</a></li>
+			</ul>
+
+			<? */
+    }
+
+	public function loader ()
+	{
+		define('BACKPRESS_PATH','');
+		
+		include_once(TENTACLE_LIB.'/backpress/functions.core.php');
+		
+		include_once(TENTACLE_LIB.'backpress/class.bp-log.php');
+		include_once(TENTACLE_LIB.'backpress/class.bp-roles.php');
+		include_once(TENTACLE_LIB.'backpress/class.bp-sql-schema-parser.php');
+		include_once(TENTACLE_LIB.'backpress/class.bp-user.php');
+		include_once(TENTACLE_LIB.'backpress/class.bpdb-multi.php');
+		include_once(TENTACLE_LIB.'backpress/class.bpdb.php');
+		include_once(TENTACLE_LIB.'backpress/class.ixr.php');
+		include_once(TENTACLE_LIB.'backpress/class.mailer-smtp.php');
+		include_once(TENTACLE_LIB.'backpress/class.mailer.php');
+		include_once(TENTACLE_LIB.'backpress/class.passwordhash.php');
+		include_once(TENTACLE_LIB.'backpress/class.wp-ajax-response.php');
+		include_once(TENTACLE_LIB.'backpress/class.wp-auth.php');
+		include_once(TENTACLE_LIB.'backpress/class.wp-dependencies.php');
+		include_once(TENTACLE_LIB.'backpress/class.wp-error.php');
+		include_once(TENTACLE_LIB.'backpress/class.wp-http.php');
+		include_once(TENTACLE_LIB.'backpress/class.wp-pass.php');
+		include_once(TENTACLE_LIB.'backpress/class.wp-scripts.php');
+		include_once(TENTACLE_LIB.'backpress/class.wp-styles.php');
+		include_once(TENTACLE_LIB.'backpress/class.wp-taxonomy.php');
+		include_once(TENTACLE_LIB.'backpress/class.wp-users.php');
+		include_once(TENTACLE_LIB.'backpress/functions.bp-options.php');
+		include_once(TENTACLE_LIB.'backpress/functions.compat.php');
+		include_once(TENTACLE_LIB.'backpress/functions.formatting.php');
+		include_once(TENTACLE_LIB.'backpress/functions.kses.php');
+		include_once(TENTACLE_LIB.'backpress/functions.plugin-api.php');
+		include_once(TENTACLE_LIB.'backpress/functions.shortcodes.php');
+		include_once(TENTACLE_LIB.'backpress/functions.wp-cron.php');
+		include_once(TENTACLE_LIB.'backpress/functions.wp-object-cache.php');
+		include_once(TENTACLE_LIB.'backpress/functions.wp-scripts.php');
+		include_once(TENTACLE_LIB.'backpress/functions.wp-styles.php');
+		include_once(TENTACLE_LIB.'backpress/functions.wp-taxonomy.php');
+		include_once(TENTACLE_LIB.'backpress/interface.bp-options.php');
+
+		// Only include one of these two
+		include_once(TENTACLE_LIB.'backpress/loader.wp-object-cache-memcached.php');
+		//include_once(TENTACLE_LIB.'backpress/loader.wp-object-cache.php');
+		
+		// Load an array of assets
+		
+		// Read what has been requested
+		
+		// Look in the assets folder
+		
+		//load the content and output to this files
+	}
 }// END Dev
