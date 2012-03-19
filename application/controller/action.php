00403409d53f4c7707b31621d9ed7e260a4a4e5a
Adam Patterson <hello@adampatterson.ca>
2012-03-19 11:12:43 -0600

    upload image controller

--- application/controller/action.php
+++ application/controller/action.php
@@ -564,10 +564,54 @@ class action_controller
     /**
      * Upload
      * ----------------------------------------------------------------------------------------------*/
-
-    public function upload_image ()
+	/*
+	 * jQuery File Upload Plugin PHP Example 5.7
+	 * https://github.com/blueimp/jQuery-File-Upload
+	 *
+	 * Copyright 2010, Sebastian Tschan
+	 * https://blueimp.net
+	 *
+	 * Licensed under the MIT license:
+	 * http://www.opensource.org/licenses/MIT
+	 */
+    public function upload_media ()
     {
+		$target_folder = STORAGE_DIR.'images/'; // Relative to the root
+		
+		error_reporting(E_ALL | E_STRICT);
 
+		tentacle::library('','upload.class');
+
+		$upload_handler = new UploadHandler();
+
+		header('Pragma: no-cache');
+		header('Cache-Control: no-store, no-cache, must-revalidate');
+		header('Content-Disposition: inline; filename="files.json"');
+		header('X-Content-Type-Options: nosniff');
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Methods: OPTIONS, HEAD, GET, POST, PUT, DELETE');
+		header('Access-Control-Allow-Headers: X-File-Name, X-File-Type, X-File-Size');
+
+		switch ($_SERVER['REQUEST_METHOD']) {
+		    case 'OPTIONS':
+		        break;
+		    case 'HEAD':
+		    case 'GET':
+		        $upload_handler->get();
+		        break;
+		    case 'POST':
+		        if (isset($_REQUEST['_method']) && $_REQUEST['_method'] === 'DELETE') {
+		            $upload_handler->delete();
+		        } else {
+		            $upload_handler->post();
+		        }
+		        break;
+		    case 'DELETE':
+		        $upload_handler->delete();
+		        break;
+		    default:
+		        header('HTTP/1.1 405 Method Not Allowed');
+		}
     }
 
 
