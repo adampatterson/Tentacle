f1de5d714ade8862b93b63ebf440638940da6227
Adam Patterson <hello@adampatterson.ca>
2012-04-19 17:08:36 -0600

    Moved Design to Home Template
    Added Blog template, and post template.
    
    Hooked up some more template tags.

--- tentacle/themes/marketing/part-footer.php
+++ tentacle/themes/marketing/part-footer.php
@@ -1,19 +1,52 @@
-<!-- Google Analytics -->
-<script type="text/javascript">
+	
+	<footer>
+		<div class="container">
+			<div class="row bump">
+				<div class="span6">
+					<h2>Let us know what you think.</h2>
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-403336-24']);
-  _gaq.push(['_setDomainName', 'none']);
-  _gaq.push(['_setAllowLinker', true]);
-  _gaq.push(['_trackPageview']);
+					<ul class="unstyled">
+						<li class="lead"><a data-toggle="modal" href="#myModal">Mailing List</a></li>
+						<li class="lead"><a href="mailto:hello@tentaclecms.com">hello@tentaclecms.com</a></li>
+						<li class="lead"><a href="https://twitter.com/#!/TentacleCMS">@TentacleCMS</a></li>
+					</ul>
+				</div>
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+				<div class="span6">	
+					<p class="pull-right"><img src="<?= PATH ?>/assets/img/adam-patterson.png" alt="" /></p>
+				</div>
+<? /*			
+				<div class="span4">
+					<h3>Blog</h3>
+					<? dashboard_feed("http://tentaclecms.com/blog/feed/", 4, true ); ?>
+				</div>
+*/?>
+			</div>
+		</div> <!-- /container -->
+	</footer>
 
-</script>
-<!-- End Google Analytics -->
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="<?= PATH ?>/assets/js/bootstrap.min.js"></script>
+	<script src="<?= PATH ?>/assets/js/application.js"></script>
+	
+	<a href="https://github.com/adampatterson/Tentacle/tree/beta-wip" class="visible-desktop"><img style="position: absolute; top: 60px; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/abad93f42020b733148435e2cd92ce15c542d320/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub"></a>
+
+	<!-- Google Analytics -->
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-403336-24']);
+	  _gaq.push(['_setDomainName', 'none']);
+	  _gaq.push(['_setAllowLinker', true]);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+	<!-- End Google Analytics -->
 </body>
 </html>
--- tentacle/themes/marketing/type-post.php
+++ tentacle/themes/marketing/type-post.php
@@ -3,19 +3,16 @@
 Type: Post
 */
 if( !defined( 'SCAFFOLD' ) ):?>
-<? load_part('header',array('title'=>$data->title,'assets'=>'default')); ?>
-<div class="row-fluid">
-	<div class="span3">
-		<div class="well sidebar-nav">
-			<? load_part( 'sidebar' ); ?>
-		</div><!--/.well -->
-	</div><!--/span3-->
-	<div class="span9">
-		<div class="hero-unit">
-			<h1><?= $data->title; ?></h1>
-			<?= render_content( $data->content ); ?>
-		</div><!-- /hero-unit -->
-	</div><!--/span9-->
-</div><!--/row-->
+<? load_part('header',array('title'=>$data->title,'assets'=>'marketing')); ?>
+<div class="container">
+	<div class="row">
+		<div class="span12">
+			<div class="hero-unit">
+				<h1><?= $data->title; ?></h1>
+				<?= render_content( $data->content ); ?>
+			</div><!-- /hero-unit -->
+		</div><!--/span9-->
+	</div><!--/row-->
+</div><!-- /container -->
 <? load_part('footer'); ?> 
 <? endif; ?>
