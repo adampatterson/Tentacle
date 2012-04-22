c897b88d2a033af8ab753dd9aacd79b238134c9d
Adam Patterson <hello@adampatterson.ca>
2012-04-21 21:37:34 -0600

    close #85

--- application/helper/is.php
+++ application/helper/is.php
@@ -41,4 +41,18 @@
 	function is_tag() {}
 	function is_tax() {}
 	function is_admin_bar_showing() {}
+		
+	function is_mobile() 
+	{
+		$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+		$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+		$palmpre = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
+		$berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
+		$ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+
+		if ($iphone || $android || $palmpre || $ipod || $berry == true) 
+		{ 
+			echo 'yes';
+		}
+	}
 
