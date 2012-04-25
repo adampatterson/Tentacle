396c0bb5d7c7647fe03e7cfcd898f42f3713ec8f
Adam Patterson <hello@adampatterson.ca>
2012-04-25 11:02:05 -0600

    Updated variables.

--- tentacle/themes/marketing/template-blog.php
+++ tentacle/themes/marketing/template-blog.php
@@ -14,25 +14,25 @@ if( !defined( 'SCAFFOLD' ) ):?>
 <div class="container bump-top">
 		<?
 	// Loop all of the blog posts.
-	foreach ($data as $post): $user_meta = $user->get_meta ( $post->author ); ?>
+	foreach ($posts as $post): $author_meta = $author->get_meta ( $post->author ); ?>
 		<div class="row bump">
 			<div class="span12">
 
-			<h2 class="title"><a href="<?= BASE_URL.$post->uri ?>"><?= $post->title?></a></h2>
+			<h2 class="title"><a href="<? _e(BASE_URL.$post->uri) ?>"><? _e($post->title) ?></a></h2>
 			
 			<hr />
 			
 			<?= render_content( $post->content ); ?>
-			<small>Created by: <?= $user_meta -> first_name;?> <?= $user_meta -> last_name;?></small>
+			<small>Created by: <? _e($author_meta->first_name.' '.$author_meta->last_name) ?></small>
 
 			<p><small>Posted in: 
 				<? foreach( $category->get_relations( $post->id ) as $relation ): ?>
-					 <a href="#<?=$relation->slug ?>"><?= $relation->name ?></a>
+					 <a href="#<?=$relation->slug ?>"><?_e($relation->name) ?></a>
 				<? endforeach; ?>
 			</small></p>
 			<p><small>Tags: 
 				<? foreach( $relations = $tag->get_relations( $post->id ) as $relation ): ?>
-					 <a href="#<?=$relation->slug ?>"><?= $relation->name ?></a>
+					 <a href="#<?=$relation->slug ?>"><?_e($relation->name) ?></a>
 				<? endforeach; ?>
 			</small></p>
 			</div><!--/span9-->
--- tentacle/themes/marketing/type-post.php
+++ tentacle/themes/marketing/type-post.php
@@ -3,13 +3,14 @@
 Type: Post
 */
 if( !defined( 'SCAFFOLD' ) ):?>
-<? load_part('header',array('title'=>$data->title,'assets'=>'marketing')); ?>
+<? load_part('header',array('title'=>$post->title,'assets'=>'marketing')); ?>
 <div class="container">
 	<div class="row">
 		<div class="span12">
 			<div class="hero-unit">
-				<h1><?= $data->title; ?></h1>
-				<?= render_content( $data->content ); ?>
+				<h1><?= $post->title; ?></h1>
+				<hr />
+				<?= render_content( $post->content ); ?>
 			</div><!-- /hero-unit -->
 		</div><!--/span9-->
 	</div><!--/row-->
