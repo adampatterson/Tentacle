5c11e9e8d5920972074c256dddbc88d205cf9595
Adam Patterson <hello@adampatterson.ca>
2012-04-25 11:01:07 -0600

    Removed the old marketing theme.

--- tentacle/themes/mailing/functions.php
+++
@@ -1,5 +0,0 @@
-<?
-/**
- * Functions functions
- * @todo plan out how and what this file should interact with the theme as well as the CMS.
-**/
--- tentacle/themes/mailing/part-header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en"> 
-<head>
-<meta charset="utf-8"> 
-<title>Tentacle Admin - <?= $title?></title>
-<? assets::render($assets); ?>
-</head>
-<body class="login">
-	<div id="login-wrapper">
