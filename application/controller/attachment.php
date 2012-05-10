8f706cfd5faf8524fadcd062761a7f55b62e2dac
Adam Patterson <hello@adampatterson.ca>
2012-05-10 16:52:33 -0600

    Process images ( three sizes + square )

--- application/controller/attachment.php
+++ application/controller/attachment.php
@@ -12,16 +12,79 @@ class attachment_controller {
 		$file_meta = explode('.', $file['file'] );
 		
         if( file_exists( $file_path ) )
-        {
-			$size = 250;            
-
+        {         
 			$image = new image( IMAGE_DIR.$file['file'] );
-            $image->resize($size,NULL);
 			
-			$size = '_'.$size;
-			
-            $image->save( IMAGE_DIR.$file_meta[0].$size.'.'.$file_meta[1] );
-            $image->close();
+			if ($image->width > $image->height ) {
+				// Thumb
+				$image = new image( IMAGE_DIR.$file['file'] );
+				
+				$image->resize( IMAGE_T,NULL );
+				
+				$size = '_'.IMAGE_T;
+
+	            $image->save( IMAGE_DIR.$file_meta[0].$size.'.'.$file_meta[1] );
+	            $image->close();
+	
+				// Medium
+				$image = new image( IMAGE_DIR.$file['file'] );
+				
+				$image->resize( IMAGE_M, NULL );
+				
+				$size = '_'.IMAGE_M;
+
+	            $image->save( IMAGE_DIR.$file_meta[0].$size.'.'.$file_meta[1] );
+	            $image->close();
+	
+				// Medium
+				$image = new image( IMAGE_DIR.$file['file'] );
+				
+				$image->resize( IMAGE_L,NULL );
+				
+				$size = '_'.IMAGE_L;
+
+	            $image->save( IMAGE_DIR.$file_meta[0].$size.'.'.$file_meta[1] );
+	            $image->close();
+			} else {
+				// Thumb
+				$image = new image( IMAGE_DIR.$file['file'] );
+				
+				$image->resize( NULL,IMAGE_T );
+				
+				$size = '_'.IMAGE_T;
+
+	            $image->save( IMAGE_DIR.$file_meta[0].$size.'.'.$file_meta[1] );
+	            $image->close();
+	
+				// Medium
+				$image = new image( IMAGE_DIR.$file['file'] );
+				
+				$image->resize(NULL,IMAGE_M);
+				
+				$size = '_'.IMAGE_M;
+
+	            $image->save( IMAGE_DIR.$file_meta[0].$size.'.'.$file_meta[1] );
+	            $image->close();
+	
+				// Medium
+				$image = new image( IMAGE_DIR.$file['file'] );
+				
+				$image->resize(NULL,IMAGE_L);
+				
+				$size = '_'.IMAGE_L;
+
+	            $image->save( IMAGE_DIR.$file_meta[0].$size.'.'.$file_meta[1] );
+	            $image->close();
+			}
+				// Medium
+				$image = new image( IMAGE_DIR.$file['file'] );
+				
+				$image->dynamic_resize(IMAGE_T,IMAGE_T);
+				
+				$size = '_sq';
+
+	            $image->save( IMAGE_DIR.$file_meta[0].$size.'.'.$file_meta[1] );
+	            $image->close();
         }
 
     }// END index
