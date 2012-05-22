25a22e06992995afe13ad486034ad82265a2ef36
Adam Patterson <hello@adampatterson.ca>
2012-05-21 22:32:44 -0600

    Media Model for inserting images into the DB
    Needs a check for duplicates.

--- application/model/media.php
+++ application/model/media.php
@@ -1,9 +1,52 @@
 <?
 class media_model
 {
-    public function upload ()
+    public function add( $file )
     {
+		$file_meta = explode('.', $file );
+		
+		$file_name = $file_meta[0];
 
+		$slug			= sanitize($file_name);
+		$author 		= user::id();
+
+		// Run content through HTMLawd and Samrty Text
+		$media          = db('media');
+
+		$row = $media->insert(array(
+			'uri'			=> IMAGE_URI.$file,
+			'slug'			=> $slug,
+			'title'			=> $file_name,
+			'date'			=> time(),
+			'alt'			=> $file_name,
+			'count'			=> 1,
+			'type'			=> 'image',
+			'author'		=> $author
+		));
+
+		load::helper('image');
+		process_image($file, TRUE);
     }
 
+	public function update( $id )
+	{
+		// Run content through HTMLawd and Samrty Text
+		$media          = db('media');
+
+		$row = $media->update(array(
+			'uri'			=> $uri,
+			'slug'			=> $slug,
+			'title'			=> $title,
+			'caption'		=> $caption,
+			'description'	=> $description,
+			'date'			=> time(),
+			'exif'			=> $exif,
+			'alt'			=> $alt,
+			'count'			=> $count,
+			'type'			=> $type,
+			'author'		=> $author
+			))		
+			->where( 'id', '=', $id )
+			->execute();
+	}
 }
