333fe782dc79938ecf0012cf6cf0cb31ecb318a2
Adam Patterson <hello@adampatterson.ca>
2012-06-15 15:45:49 -0600

    Added user voice to the marketing site footer

--- tentacle/themes/marketing/partials/footer.php
+++ tentacle/themes/marketing/partials/footer.php
@@ -45,6 +45,14 @@
 	  })();
 
 	</script>
+	<script type="text/javascript">
+	  var uvOptions = {};
+	  (function() {
+	    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+	    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/vwlLYbaRRDojY33J3wRaHw.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+	  })();
+	</script>
 	<!-- End Google Analytics -->
 </body>
 </html>
