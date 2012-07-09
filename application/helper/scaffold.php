41568176da074eb20d731854cb14965e2aa24b72
Adam Patterson <hello@adampatterson.ca>
2012-07-09 17:30:20 -0600

    fixed inflector ( removed class for functions )

--- application/helper/scaffold.php
+++ application/helper/scaffold.php
@@ -25,13 +25,11 @@ class  Scaffold
     static public function processThis( $scaffold_data ) 
     {  
         $return_data = "";
-      
-		$inflector = new inflector();
 
         if ( $scaffold_data[ 'display' ] == 'admin' ):
 			foreach ($scaffold_data as $input):
 			
-				$input_name = $inflector->underscore( $inflector->camelize( $input['name']) );
+				$input_name = underscore( camelize( $input['name']) );
 
 	            //print_r($input);
 	            /*  
@@ -104,13 +102,11 @@ class  Scaffold
 		return false;
 
         $return_data = "";
-
-		$inflector = new inflector();
 		
         if ( $data[ 'display' ] != 'front' ):
 			foreach ($data as $input):
 			
-				$input_name =$inflector->underscore( $inflector->camelize( $input['name']) );
+				$input_name = underscore( camelize( $input['name']) );
 				
 	            if ($input['input'] == 'input'):
 	                switch($input['type']) {
