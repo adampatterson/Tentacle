300807c31cd573a60d45d005dae243c30b475122
Adam Patterson <hello@adampatterson.ca>
2012-07-29 12:42:22 -0600

    #50 cleaned up snippet page

--- application/view/admin/snippets/snippets_delete.php
+++ application/view/admin/snippets/snippets_delete.php
@@ -2,34 +2,38 @@
 <?	load::view('admin/template-sidebar');?>
 <div id="wrap">
 		<div id="post-body">
-				<div class="one-full">
-			     <h1 class='title'><img src="<?=ADMIN_URL;?>images/icons/icon_pages_32.png" alt="" /> Delete snippet</h1>
-					<div class="one-half">
-						<div class="table">
-							<form action="<?= BASE_URL ?>action/delete_snippet/<?= $id ?>" method="post">
-								<fieldset>
-									<p>You have chosen to delete <strong><?= $snippet->name; ?>:</strong></p>
-									<p>
-										<label>
-										<input id="delete_user" type="checkbox" value="delete" name="delete_user">
-										Confirm
-										</label>
-									</p>
-									<input type="hidden" value="" name="delete_user">
-								</fieldset>
-								<div class="actions">
-									<input type="submit" value="Delete User" class="btn medium primary" />
-								<a href="#" class="red">Cancel</a>
-								</div>
-							</form>
-						</div>
-					</div>
-					<div class="one-half">
-						<div class="table">
-							&nbsp;
-						</div>
+				
+			<div class="one-full">
+		    	
+				<h1 class='title'><img src="<?=ADMIN_URL;?>images/icons/icon_pages_32.png" alt="" /> Delete "<?= $snippet->name; ?>" snippet</h1>
+				
+				<div class="one-half">
+					<div class="table">
+						<form action="<?= BASE_URL ?>action/delete_snippet/<?= $id ?>" method="post">
+							<fieldset>
+								<p>
+									<label>
+									<input id="delete_user" type="checkbox" value="delete" name="delete_user">
+									Confirm
+									</label>
+								</p>
+								<input type="hidden" value="" name="delete_user">
+							</fieldset>
+							<div class="form-actions">
+								<input type="submit" value="Delete User" class="btn btn-primary btn-large" />
+								<a href="<?=ADMIN;?>snippets_manage/" class="red">Cancel</a>
+							</div>
+						</form>
 					</div>
 				</div>
+				
+				<div class="one-half">
+					<div class="table">
+						&nbsp;
+					</div>
+				</div>
+				
+			</div>
 				
 		</div><!-- #post-body -->
 </div>
