bd0afab6f20e1a6c7f66b17bed34cf897b9d0c63
Adam Patterson <hello@adampatterson.ca>
2012-09-10 16:16:22 -0600

    Moved the function into the Get helper.

--- application/helper/get.php
+++ application/helper/get.php
@@ -134,6 +134,25 @@
 		return $url;
 	}
 	
+	
+	/**
+	* Function: get_snippet
+	*	Function call passing the $slug to retrieve the snippet.
+	*
+	* Parameters:
+	*	$slug - String
+	*
+	* Returns:
+	*	String
+	*/
+	function get_snippet( $slug ) {
+		$snippet = load::model( 'snippet' );
+		$snippet_single = $snippet->get_slug( $slug );
+
+		return $snippet_single->content;
+		}
+		
+		
 	function get_next_post () {}
 	function get_previous_post () {}
 	function get_post_status () {}
