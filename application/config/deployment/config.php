858fcb22884a9bd65476fce20ea7adfa95165e49
Adam Patterson <hello@adampatterson.ca>
2012-09-10 23:12:00 -0600

    cleaned up some helpers

--- application/config/deployment/config.php
+++ application/config/deployment/config.php
@@ -9,7 +9,7 @@ config::set( 'autoload_library', array( 'db', 'session', 'user', 'url', 'paginat
 if ( strpos( BASE_URI,'install' ) == true ) {
 	config::set( 'autoload_helper', array(  ) ); 	
 } else {
-	config::set( 'autoload_helper', array( 'theme', 'scaffold', 'state', 'get', 'is', 'tentacle', 'navigation', 'gravatar', 'shortcode', 'inflector', 'string', 'user', 'snippet') );
+	config::set( 'autoload_helper', array( 'theme', 'scaffold', 'state', 'get', 'is', 'tentacle', 'upgrade', 'navigation', 'gravatar', 'shortcode', 'inflector', 'string', 'user') );
 }
 
 /* Sessions */
