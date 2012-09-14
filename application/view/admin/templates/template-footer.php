8ec9912f68ffdfd46912db6c8c73da86b85ac1d9
Adam Patterson <hello@adampatterson.ca>
2012-09-14 07:30:35 -0600

    Footer cleanup

--- application/view/admin/templates/template-footer.php
+++ application/view/admin/templates/template-footer.php
@@ -1,16 +1,22 @@
 </div>
 <!-- #body-wrapper -->
 <? //render_debug(); ?>
-<div class="nav nav-pills navbar-fixed-bottom">
-	<div class="container-full">
-		<div class="one-half"><p><a href="https://github.com/adampatterson/Tentacle/wiki" target="_blank">Documentation</a> | 
-			<a href="https://github.com/adampatterson/Tentacle/issues" target="_blank">Feedback</a> | 
-			<a href="https://github.com/adampatterson/Tentacle/wiki/Credits" target="_blank">Credits</a> |  
-			<? upgrade::check_core_version_footer(); ?></p></div>
-		<div class="one-half"><p class="pull-right"><span>Thanks for creating with</<span> <a href="http://tentaclecms.com"><img src="<?= TENTACLE_URL.'/admin/images/tentacle_logo_footer.png' ?>" alt="Tentacle CMS" /></a></p></div>
+<footer>
+<div class="nav navbar-fixed-bottom">
+	<div class="navbar-inner">
+		<div class="container-fluid">
+			<p class="pull-left"><a href="https://github.com/adampatterson/Tentacle/wiki" target="_blank">Documentation</a> | 
+				<a href="https://github.com/adampatterson/Tentacle/issues" target="_blank">Feedback</a> | 
+				<a href="https://github.com/adampatterson/Tentacle/wiki/Credits" target="_blank">Credits</a> | 
+				<? upgrade::check_core_version_footer(); ?>
+			<p>
+			<p class="pull-right">
+				<span>Thanks for creating with</<span> <a href="http://tentaclecms.com"><img src="<?= TENTACLE_URL.'/admin/images/tentacle_logo_footer.png' ?>" alt="Tentacle CMS" /></a>
+			</p>
+		</div>
 	</div>
 </div>
-
+</footer>
 <? if(user_editor() == 'wysiwyg'): ?>
 	<script type="text/javascript" src="<?=TENTACLE_JS; ?>tiny_mce/jquery.tinymce.js"></script>
 <? else: ?>
