59acfc36722d6dfb017c95f291dc3dc746c86bed
Adam Patterson <hello@adampatterson.ca>
2012-10-20 16:36:28 -0600

    Added Olark, Added the module settings view.

--- application/view/admin/settings/settings_modules.php
+++ application/view/admin/settings/settings_modules.php
@@ -4,27 +4,89 @@
 	<div class="one-full">
 		<div class="title">
 			<h1 class="align-left"><img src="<?=ADMIN_URL;?>images/icons/icon_pages_32.png" alt="" /> Modules</h1>
-			<a href="<?=ADMIN;?>content_add_page/" class="btn btn-primary">Install new Modules</a>
+			<a href="#" class="btn btn-primary">Install new Modules</a>
 		</div>
 		<form action="<?= BASE_URL ?>action/udpate_settings_post/" method="post" class="form-stacked">
 			
 			<input type="hidden" name="history" value="<?= CURRENT_PAGE ?>"/>
 		
 			<h2>Active Modules</h2>
-			<div class="well">
-				<p>These would be modules that are currently installed.</p>
-			</div>
+			<table width="100%" border="0" cellspacing="0" cellpadding="0" class="active_modules">
+				<thead>
+					<tr>
+						<th>Title</th>
+						<th>Description</th>
+						<th>Version</th>
+						<th>Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					<? foreach ($modules['enabled_modules'] as $module_array):
+					$module = (object)$module_array;
+
+					?>
+					<tr>
+						<td>
+							<strong><?= $module->name ?></strong>
+						</td>
+						<td >
+							<?= $module->description ?> <br /><strong>by</strong> <?= $module->author['link'] ?>
+						</td>
+						<td >
+							<span class="badge"><?= $module->version ?></span>
+						</td>
+						<td>
+							<a href="<?= BASE_URL ?>action/disable_module/<?= $module->classes[0] ?>" class="btn btn-small btn-danger">Disable</a>
+							<? if ($module->help != ''): ?>
+								<a href="<?= ADMIN ?>content_update_page/<?= $page->id;?>" class="btn btn-small">Help</a>
+							<? endif; ?>	
+						</td>
+					</tr>
+					<?endforeach;?>
+				</tbody>
+			</table>
 		
 			<h2>Inactive Modules</h2>
-			<div class="well">
-				<p>Modules that are not installed, but that have been downloaded.</p>
-			</div>
-		
+			<table width="100%" border="0" cellspacing="0" cellpadding="0" class="inactive_modules">
+				<thead>
+					<tr>
+						<th>Title</th>
+						<th>Description</th>
+						<th>Version</th>
+						<th>Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+					<? foreach ($modules['disabled_modules'] as $module_array):
+					$module = (object)$module_array;
+
+					?>
+					<tr>
+						<td>
+							<strong><?= $module->name ?></strong>
+						</td>
+						<td >
+							<?= $module->description ?> <br /><strong>by</strong> <?= $module->author['link'] ?>
+						</td>
+						<td >
+							<span class="badge"><?= $module->version ?></span>
+						</td>
+						<td>
+							<a href="<?= BASE_URL ?>action/enable_module/<?= $module->classes[0] ?>" class="btn btn-small btn-success">Activate</a>
+							<? if ($module->help != ''): ?>
+								<a href="<?= ADMIN ?>content_update_page/<?= $page->id;?>" class="btn btn-small">Help</a>
+							<? endif; ?>	
+						</td>
+					</tr>
+					<?endforeach;?>
+				</tbody>
+			</table>
+		<? /* 
 			<h2>Updates Available</h2>
 			<div class="well">
 				<p>Modules with updates.</p>
 			</div>
-			
+		*/?>	
 		</form>
 	</div>
 </div><!-- #wrap -->
