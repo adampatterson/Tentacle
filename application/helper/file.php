e268aa53d0e34841cc6e54cf9e1f4de747de0363
Adam Patterson <hello@adampatterson.ca>
2012-10-27 17:05:50 -0600

    documented  get_url_contents()

--- application/helper/file.php
+++ application/helper/file.php
@@ -195,14 +195,25 @@ function recurse_dirsize( $directory ) {
 }
 
 
-function get_url_contents ( $Url ) {
-    if (!function_exists('curl_init')){ 
+/**
+* Function: get_url_contents
+* Wrapper function for CURL, alternative to the some times disabled function file_get_contents() on some hosting environments.
+*
+* Parameters:
+*	$url - string
+*
+* Returns:
+*	$output - curl contents
+*/
+function get_url_contents ( $url ) {
+    if (!function_exists('curl_init'))
         die('CURL is not installed!');
-    }
+
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $Url);
+    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $output = curl_exec($ch);
     curl_close($ch);
+
     return $output;
 }
