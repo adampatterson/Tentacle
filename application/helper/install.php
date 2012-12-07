0ab63b3571cd5721ea4fbc4238bad4226c1ce5a2
Adam Patterson <hello@adampatterson.ca>
2012-12-07 13:59:59 -0700

    Fix for install

--- application/helper/install.php
+++ application/helper/install.php
@@ -15,3 +15,24 @@ function write_htaccess()
 		fclose($fp);
 	endif;
 }
+
+/**
+ * Function: maybe_encoded
+ *	Unserialize value only if it was serialized.
+ *	JSON Decode value only if it was JSON encoded
+ *
+ * Parameters:
+ *	$original - string $original Maybe unserialized or json encoded
+ *
+ * Returns:
+ *	$original - mixed Unserialized/json_decoded data.
+ */
+function maybe_encoded( $original ) {
+    if ( is_serialized( $original ) ) // don't attempt to unserialize data that wasn't serialized going in
+        return @unserialize( $original );
+
+    if ( is_json( $original ) ) // don't attempt to unserialize data that wasn't serialized going in
+        return @json_decode( $original );
+
+    return $original;
+}
