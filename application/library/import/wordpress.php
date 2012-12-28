f1dbc2c9d2512f04a517c8440a953f4b56a81c39
Adam Patterson <hello@adampatterson.ca>
2012-12-28 13:35:22 -0700

    don't import or process comments

--- application/library/import/wordpress.php
+++ application/library/import/wordpress.php
@@ -137,7 +137,7 @@ class WXR_Parser {
                     'value' => (string) $meta->meta_value
                 );
             }
-
+            /*
             foreach ( $wp->comment as $comment ) {
                 $meta = array();
                 if ( isset( $comment->commentmeta ) ) {
@@ -164,7 +164,7 @@ class WXR_Parser {
                     'comment_user_id' => (int) $comment->comment_user_id,
                     'commentmeta' => $meta,
                 );
-            }
+            }  */
 
             $posts[] = $post;
         }
@@ -178,125 +178,5 @@ class WXR_Parser {
             'base_url' => $base_url,
             'version' => $wxr_version
         );
-    }
-}
-
-/**
- * If fetching attachments is enabled then attempt to create a new attachment
- *
- * @param array $post Attachment post details from WXR
- * @param string $url URL to fetch attachment from
- * @return int|WP_Error Post ID on success, WP_Error otherwise
- */
-function process_attachment( $post, $url ) {
-    if ( ! $this->fetch_attachments )
-        return new WP_Error( 'attachment_processing_error',
-            __( 'Fetching attachments is not enabled', 'wordpress-importer' ) );
-
-    // if the URL is absolute, but does not contain address, then upload it assuming base_site_url
-    if ( preg_match( '|^/[\w\W]+$|', $url ) )
-        $url = rtrim( $this->base_url, '/' ) . $url;
-
-    $upload = $this->fetch_remote_file( $url, $post );
-    if ( is_wp_error( $upload ) )
-        return $upload;
-
-    if ( $info = wp_check_filetype( $upload['file'] ) )
-        $post['post_mime_type'] = $info['type'];
-    else
-        return new WP_Error( 'attachment_processing_error', __('Invalid file type', 'wordpress-importer') );
-
-    $post['guid'] = $upload['url'];
-
-    // as per wp-admin/includes/upload.php
-    $post_id = wp_insert_attachment( $post, $upload['file'] );
-    wp_update_attachment_metadata( $post_id, wp_generate_attachment_metadata( $post_id, $upload['file'] ) );
-
-    // remap resized image URLs, works by stripping the extension and remapping the URL stub.
-    if ( preg_match( '!^image/!', $info['type'] ) ) {
-        $parts = pathinfo( $url );
-        $name = basename( $parts['basename'], ".{$parts['extension']}" ); // PATHINFO_FILENAME in PHP 5.2
-
-        $parts_new = pathinfo( $upload['url'] );
-        $name_new = basename( $parts_new['basename'], ".{$parts_new['extension']}" );
-
-        $this->url_remap[$parts['dirname'] . '/' . $name] = $parts_new['dirname'] . '/' . $name_new;
-    }
-
-    return $post_id;
-}
-
-/**
- * Attempt to download a remote file attachment
- *
- * @param string $url URL of item to fetch
- * @param array $post Attachment details
- * @return array|WP_Error Local file location details on success, WP_Error otherwise
- */
-function fetch_remote_file( $url, $post ) {
-    // extract the file name and extension from the url
-    $file_name = basename( $url );
-
-    // get placeholder file in the upload dir with a unique, sanitized filename
-    $upload = wp_upload_bits( $file_name, 0, '', $post['upload_date'] );
-    if ( $upload['error'] )
-        return new WP_Error( 'upload_dir_error', $upload['error'] );
-
-    // fetch the remote url and write it to the placeholder file
-    $headers = wp_get_http( $url, $upload['file'] );
-
-    // request failed
-    if ( ! $headers ) {
-        @unlink( $upload['file'] );
-        return new WP_Error( 'import_file_error', __('Remote server did not respond', 'wordpress-importer') );
-    }
-
-    // make sure the fetch was successful
-    if ( $headers['response'] != '200' ) {
-        @unlink( $upload['file'] );
-        return new WP_Error( 'import_file_error', sprintf( __('Remote server returned error response %1$d %2$s', 'wordpress-importer'), esc_html($headers['response']), get_status_header_desc($headers['response']) ) );
-    }
-
-    $filesize = filesize( $upload['file'] );
-
-    if ( isset( $headers['content-length'] ) && $filesize != $headers['content-length'] ) {
-        @unlink( $upload['file'] );
-        return new WP_Error( 'import_file_error', __('Remote file is incorrect size', 'wordpress-importer') );
-    }
-
-    if ( 0 == $filesize ) {
-        @unlink( $upload['file'] );
-        return new WP_Error( 'import_file_error', __('Zero size file downloaded', 'wordpress-importer') );
-    }
-
-    $max_size = (int) $this->max_attachment_size();
-    if ( ! empty( $max_size ) && $filesize > $max_size ) {
-        @unlink( $upload['file'] );
-        return new WP_Error( 'import_file_error', sprintf(__('Remote file is too large, limit is %s', 'wordpress-importer'), size_format($max_size) ) );
-    }
-
-    // keep track of the old and new urls so we can substitute them later
-    $this->url_remap[$url] = $upload['url'];
-    $this->url_remap[$post['guid']] = $upload['url']; // r13735, really needed?
-    // keep track of the destination if the remote url is redirected somewhere else
-    if ( isset($headers['x-final-location']) && $headers['x-final-location'] != $url )
-        $this->url_remap[$headers['x-final-location']] = $upload['url'];
-
-    return $upload;
-}
-
-/**
- * Use stored mapping information to update old attachment URLs
- */
-function backfill_attachment_urls() {
-    global $wpdb;
-    // make sure we do the longest urls first, in case one is a substring of another
-    uksort( $this->url_remap, array(&$this, 'cmpr_strlen') );
-
-    foreach ( $this->url_remap as $from_url => $to_url ) {
-        // remap urls in post_content
-        $wpdb->query( $wpdb->prepare("UPDATE {$wpdb->posts} SET post_content = REPLACE(post_content, %s, %s)", $from_url, $to_url) );
-        // remap enclosure urls
-        $result = $wpdb->query( $wpdb->prepare("UPDATE {$wpdb->postmeta} SET meta_value = REPLACE(meta_value, %s, %s) WHERE meta_key='enclosure'", $from_url, $to_url) );
     }
 }
