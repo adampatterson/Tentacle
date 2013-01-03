a9109ee8763ead0b9b69ea12fe47d6996faa7454
Adam Patterson <hello@adampatterson.ca>
2013-01-03 16:25:59 -0700

    Moved XML helper into Common

--- application/helper/common.php
+++ application/helper/common.php
@@ -322,7 +322,7 @@ class date
 	* Returns the difference between the given timestamps or now.
 	*
 	* Parameters:
-	*     $time - Timestamp to compare to.
+	*     $when - Timestamp to compare to.
 	*     $from - Timestamp to compare from. If not specified, defaults to now.
 	*
 	* Returns:
@@ -1379,7 +1379,6 @@ function refresh($sec = 1)
 
 /**
 * Function: url_title
-* Refreshes the page every $sec
 *
 * Parameters:
 *     $url - String
@@ -1413,7 +1412,8 @@ function url_title($url)
 * Returns:
 *	HTML
 */
-function notification( ){
+function notification( )
+{
 	if ($notes = note::all()):
 		foreach ($notes as $note): ?>
 		
@@ -1427,6 +1427,71 @@ function notification( ){
 	endif;
 }
 
+
+/**
+ * Function: xml_arr
+ * Recursively converts a SimpleXML object (and children) to an array.
+ *
+ * Parameters:
+ * 		$parse - The SimpleXML object to convert into an array.
+ *
+ * Returns:
+ *     	array
+ *
+ * See Also:
+ *     <arr_xml>
+ */
+function xml_arr($parse)
+{
+    if (empty($parse))
+        return "";
+
+    $parse = (array) $parse;
+
+    foreach ($parse as &$val)
+        if (get_class($val) == "SimpleXMLElement")
+            $val = xml2arr($val);
+
+    return $parse;
+}
+
+
+/**
+ * Function: arr_xml
+ * Recursively adds an array (or object I guess) to a SimpleXML object.
+ *
+ * Parameters:
+ * 		&$object - The SimpleXML object to modify.
+ * 		$data - The data to add to the SimpleXML object.
+ *
+ * Returns:
+ *     	xml
+ *
+ * See Also:
+ *     <xml_arr>
+ */
+function arr_xml(&$object, $data)
+{
+    foreach ($data as $key => $val) {
+        if (is_int($key) and (empty($val) or (is_string($val) and trim($val) == ""))) {
+            unset($data[$key]);
+            continue;
+        }
+
+        if (is_array($val)) {
+            if (in_array(0, array_keys($val))) { # Numeric-indexed things need to be added as duplicates
+                foreach ($val as $dup) {
+                    $xml = $object->addChild($key);
+                    arr2xml($xml, $dup);
+                }
+            } else {
+                $xml = $object->addChild($key);
+                arr2xml($xml, $val);
+            }
+        } else
+            $object->addChild($key, fix($val, false, false));
+    }
+}
 
 /*
 //All major credit cards regex
