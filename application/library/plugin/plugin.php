5ac9d5b814dc4d7a7fc816f9afa5c95292bc67e1
Adam Patterson <hello@adampatterson.ca>
2013-01-12 22:34:28 -0700

    Plugins more integrated into the CMS, need to hook up and register navigation items in the Admin side.

--- application/library/plugin/plugin.php
+++ application/library/plugin/plugin.php
@@ -1,6 +1,7 @@
-<?php
+<?
+
 /**
- * Part of the Fuel framework.
+ * Based off the Event class from Fuel php 1.0
  *
  * @package    Fuel
  * @version    1.0
@@ -10,12 +11,15 @@
  * @link       http://fuelphp.com
  */
 
-class Event
+/**
+ * Class: event
+ */
+class event
 {
     /**
      * @var  array  $instances  Event_Instance container
      */
-    protected static $instances = array();
+    static $instances = array();
 
     /**
      * Event instance forge.
@@ -92,25 +96,68 @@ class Event
     }
 }
 
+
 /**
- * Event Class
+ * Function: plugin_enabled
+ * Returns whether the given plugin is enabled or not.
  *
- * @package		Fuel
- * @category	Core
- * @author		Eric Barnes
- * @author		Harro "WanWizard" Verton
+ * Parameters:
+ *     $name - The folder name of the plugin.
+ *
+ * Returns:
+ *     Whether or not the requested plugin is enabled.
+ */
+function _plugin_enabled( $name ) {
+    return in_array( $name, _enabled_plugin() );
+}
+
+
+/**
+ * Function: enabled_plugin
+ * Returns an array of enabled plugins.
+ *
+ */
+function _enabled_plugin() {
+    return unserialize(_ACTIVE_PLUGINS);
+}
+
+
+/**
+ * Function: init_extensions
+ * Initialize all Plugins
+ */
+function _init_extensions() {
+
+    //$plugins = new Plugins();
+
+    $enabled_plugins  = event::get_plugins();
+
+    foreach ($enabled_plugins['enabled_plugins'] as $plugin => $info ) {
+
+        if (!file_exists(TENTACLE_PLUGIN."/".$plugin."/".$plugin.".php")) {
+            unset($enabled_plugins[$plugin]);
+            continue;
+        }
+
+        require TENTACLE_PLUGIN."/".$plugin."/".$plugin.".php";
+
+//        foreach (YAML::load(TENTACLE_PLUGIN."/".$plugin."/info.yaml") as $key => $val)
+//            event::$instances[$plugin]->$key = (is_string($val)) ? $val : $val ;
+    }
+
+}
+
+/**
+ * Class: Event_Instance
  */
 class Event_Instance
 {
-
     /**
      * @var	array	An array of listeners
      */
     protected $_events = array();
 
     protected $_priorities = array();
-
-    // --------------------------------------------------------------------
 
     /**
      * Constructor, sets all initial events.
@@ -125,6 +172,95 @@ class Event_Instance
         }
     }
 
+    static function get_plugins(){
+        $context["enabled_plugins"] = $context["disabled_plugins"] = array();
+
+        $classes = array();
+        // Move to the plugin folder
+        chdir(TENTACLE_PLUGIN);
+        $plugin_path = array_filter(glob('*'), 'is_dir');
+
+        // Reset to the app root
+        chdir(APP_ROOT);
+
+        foreach ($plugin_path as $folder) {
+            if (!file_exists(TENTACLE_PLUGIN."/".$folder."/".$folder.".php") or !file_exists(TENTACLE_PLUGIN."/".$folder."/info.yaml")) continue;
+
+            if (!isset($classes[$folder]))
+                $classes[$folder] = array($folder);
+            else
+                array_unshift($classes[$folder], $folder);
+
+            $info = YAML::load(TENTACLE_PLUGIN."/".$folder."/info.yaml");
+
+            $info["conflicts_true"] = array();
+            $info["depends_true"] = array();
+
+            if (!empty($info["conflicts"])) {
+                $classes[$folder][] = "conflict";
+
+                foreach ((array) $info["conflicts"] as $conflict)
+                    if (file_exists(TENTACLE_PLUGIN."/".$conflict."/".$conflict.".php"))
+                        $classes[$folder][] = "conflict_".$conflict;
+            }
+
+            $dependencies_needed = array();
+            if (!empty($info["depends"])) {
+                $classes[$folder][] = "depends";
+
+                foreach ((array) $info["depends"] as $dependency) {
+                    if (!_plugin_enabled($dependency)) {
+                        if (!in_array("missing_dependency", $classes[$folder]))
+                            $classes[$folder][] = "missing_dependency";
+
+                        $classes[$folder][] = "needs_".$dependency;
+
+                        $dependencies_needed[] = $dependency;
+                    }
+
+                    $classes[$folder][] = "depends_".$dependency;
+
+                    fallback($classes[$dependency], array());
+                    $classes[$dependency][] = "depended_by_".$folder;
+                }
+            }
+
+            fallback($info["name"], $folder);
+            fallback($info["version"], "0");
+            fallback($info["url"]);
+            fallback($info["description"]);
+            fallback($info["author"], array("name" => "", "url" => ""));
+            fallback($info["help"]);
+
+            $info["description"] = $info["description"];
+            $info["description"] = preg_replace(array("/<code>(.+)<\/code>/se", "/<pre>(.+)<\/pre>/se"),
+                array("'<code>'.fix('\\1').'</code>'", "'<pre>'.fix('\\1').'</pre>'"),
+                $info["description"]);
+
+            $info["author"]["link"] = !empty($info["author"]["url"]) ?
+                '<a href="'.string::fix($info["author"]["url"]).'">'.string::fix($info["author"]["name"]).'</a>' :
+                $info["author"]["name"] ;
+
+            $category = (_plugin_enabled($folder)) ? "enabled_plugins" : "disabled_plugins" ;
+            $context[$category][$folder] = array("name" => $info["name"],
+                "version" => $info["version"],
+                "url" => $info["url"],
+                "description" => $info["description"],
+                "author" => $info["author"],
+                "help" => $info["help"],
+                "classes" => $classes[$folder],
+                "dependencies_needed" => $dependencies_needed);
+        }
+
+        foreach ($context["enabled_plugins"] as $plugin => &$attrs)
+            $attrs["classes"] = $classes[$plugin];
+
+        foreach ($context["disabled_plugins"] as $plugin => &$attrs)
+            $attrs["classes"] = $classes[$plugin];
+
+
+        return $context;
+    }
 
     static function aasort (&$array, $key) {
         $sorter=array();
