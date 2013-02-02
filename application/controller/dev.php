92bb11d3d593d8fa829ba9fc0b25182a2da0693f
Adam Patterson <hello@adampatterson.ca>
2013-02-02 14:59:56 -0700

    dingo email test

--- application/controller/dev.php
+++ application/controller/dev.php
@@ -334,45 +334,13 @@ Test two
 	public function email()
 	{
 
-        load::library('phpmailer', 'class.phpmailer' );
-
-        $mail             = new PHPMailer(); // defaults to using php "mail()"
-
-        #$mail->IsSendmail(); // telling the class to use SendMail transport
-
-        $body = 'Content';
-
-        $mail->SetFrom('hello@adampatterson.ca', 'Adam Patterson');
-
-        $mail->AddReplyTo("hello@adampatterson.ca","Adam Patterson");
-
-        $address = "hello@adampatterson.ca";
-        $mail->AddAddress($address, "Adam Patterson");
-
-        $mail->Subject    = "PHPMailer Test Subject via mail(), basic";
-
-        $mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
-
-        $mail->MsgHTML($body);
-
-//        $mail->AddAttachment("images/phpmailer.gif");      // attachment
-//        $mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
-
-        if(!$mail->Send()) {
-            echo "Mailer Error: " . $mail->ErrorInfo;
-        } else {
-            echo "Message sent!";
-        }
-
-        die;
-
 
         load::helper('email');
 		$send_email = load::model( 'email' );
 
 		$user_name    = 'user_name';
 		$password     = 'password';
-		$email        = 'adamapatterson@gmail.com';
+		$email        = 'hello@adampatterson.ca';
 	
 		$first_name   = 'Adam';
 		$last_name    = 'Patterson';
