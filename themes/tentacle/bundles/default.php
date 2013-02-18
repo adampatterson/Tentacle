385903add170e258e4a11405018ecc5c9b9730b3
Adam Patterson <hello@adampatterson.ca>
2013-02-18 16:38:10 -0700

    Tentacle theme cleanup, testing a bootswatch

--- themes/tentacle/bundles/default.php
+++ themes/tentacle/bundles/default.php
@@ -12,15 +12,7 @@ _/      _/_/_/  _/    _/      _/_/    _/_/_/    _/_/_/  _/    _/_/_/
 
 
 <script type="text/javascript" src="<?=ADMIN_JS; ?>jquery.min.js"></script>
-<link rel="stylesheet/less" type="text/css" href="<?= THEME ?>/design/less/main.less">
+<link rel="stylesheet/less" type="text/css" href="<?= THEME ?>/design/css/main.css">
 <link rel="stylesheet" type="text/css" href="<?= THEME ?>/design/css/docs.css">
-<link rel="stylesheet/less" type="text/css" href="<?= THEME ?>/design/less/bootstrap/responsive.less">
 <script type="text/javascript" src="<?= THEME ?>/js/less.js"></script>
 <script type="text/javascript" src="<?= THEME ?>/js/bootstrap.min.js"></script>
-
-<style type="text/css" media="screen">
-	body {
-      padding-top: 60px;
-      padding-bottom: 40px;
-    }
-</style>
--- themes/tentacle/partials/header.php
+++ themes/tentacle/partials/header.php
@@ -4,8 +4,8 @@
 <meta charset="utf-8"> 
 <title><?= get::option('blogname').' - '.$title ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="">
-<meta name="author" content="">
+<meta name="description" content="Tentacle CMS - Default theme">
+<meta name="author" content="Adam Patterson">
 
 <? 
 /**
@@ -21,4 +21,22 @@ render_header( );
 </head>
 
 <body <?= body_class(); ?>>
+
+<div class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container hidden-phone hidden-tablet">
+            <ul class="nav">
+                <li>
+                    <a href="<?= BASE_URL ?>" class="brand" ><?= get::option('blogname') ?></a>
+                </li>
+            </ul>
+            <ul class="nav pull-right">
+                <li>
+<!--                    <a href="#" target="_blank" >@TentacleCMS</a>-->
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
 	<div class="container">
