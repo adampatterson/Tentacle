77236daf22ae87966fc6e7d0f3d856c9c2ea5a45
Adam Patterson <hello@adampatterson.ca>
2013-02-23 23:24:53 -0700

    #111 moving stats to MySQL rather than the option API

--- application/helper/statistics.php
+++ application/helper/statistics.php
@@ -2,43 +2,62 @@
 
 function build_stats( )
 {
-    $statistics = load::model('statistics');
-
-    #stats_user_agent
-    #stats_operating_system
-    #stats_browser
-    #statas_country
-    // no string just return the key
-    // if the key is new then add the value
-    #var_dump($statistics->set('stats_test', 'Chrome 24.0.1312'));
-    var_dump($statistics->set('stats_browser', 'Chrome 25.0.1312'));
-    var_dump($statistics->get('stats_browser'));
-    var_dump($statistics->lookup('stats_browser', 'Chrome 24.0.1312'));
-
-    die;
     load::library('uaparser');
+    $ua                             = UA::parse();
 
-    $ua = UA::parse();
+    #$geo_meta                      = maybe_encoded(get::url_contents('http://geo.tentaclecms.com/'.));
+    $geo_meta                       = maybe_encoded(get::url_contents('http://geo.tentaclecms.com/'));
 
-    $geo_meta = maybe_encoded(get::url_contents('http://geo.tentaclecms.com/'.$_SERVER['REMOTE_ADDR']));
+    $statistics                     = load::model('statistics');
+    $page                           = load::model('page');
+    #$post 		                    = $page->get_by_uri( URI );
+    $post_id 		                = $page->get_by_uri( 'docs/change-log/' );
 
-    $info = array();
+    $meta                           = array();
+    $page_view                      = array();
+//    stats_user_agent
+//    stats_operating_system
+//    stats_browser
+//    statas_country
+//
+//    no string just return the key
+//    if the key is new then add the value
+//    var_dump($statistics->set('stats_test', 'Chrome 24.0.1312'));
+//
+//    var_dump($statistics->set('stats_browser', 'Chrome 25.0.1312'));
+//    var_dump($statistics->get('stats_browser'));
+//    var_dump($statistics->lookup('stats_browser', 'Chrome 24.0.1312'));
 
+    // statistics_meta
+    // Loop $meta and add the page_view ID
     if (isset($geo_meta)) {
-        $info['country'] 	= $geo_meta->countryName;
-        $info['region'] 	= $geo_meta->regionName;
-        $info['city'] 		= $geo_meta->cityName;
+        $meta['country'] 	        = $geo_meta->countryName;
+        $meta['region'] 	        = $geo_meta->regionName;
+        $meta['city'] 		        = $geo_meta->cityName;
+        $meta['latitude'] 		    = $geo_meta->latitude;
+        $meta['longitude'] 		    = $geo_meta->longitude;
     }
 
-    $info['browserFull'] = $ua->browserFull;
-    $info['browser'] = $ua->browser;
-    $info['version'] = $ua->version;
-    $info['os'] = $ua->os;
-    $info['osFull'] = $ua->osFull;
-    $info['osVersion'] = $ua->osVersion;
-    $info['url'] = HISTORY;
+    $meta['browser']                = $ua->browser;
+    $meta['browser_full']           = $ua->browserFull;
+    $meta['version']                = $ua->version;
+    $meta['user_agent']             = $ua->uaOriginal;
+    $meta['os']                     = $ua->os;
+    $meta['os_version']             = $ua->osVersion;
 
-    var_dump($info);
+    # loop the meta and rebuild an array with Key Value, where Value is the ID returned
+    $meta_id = array();
+    foreach ( $meta as $key => $value ){
+        #$page_view[$key] = $statistics->add_meta( $key, $value);
+    }
+
+    // statistics
+    // Create row, return ID
+    $page_view['uri_id']             = $post_id->id;
+    $page_view['referer']            = (isset ($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '');
+    $page_view['ip']                 = $_SERVER['REMOTE_ADDR'];
+
+    #$statistics->add($page_view);
 }
 
 function check ( $key, $array )
--- application/model/statistics.php
+++ application/model/statistics.php
@@ -1,10 +1,94 @@
 <?
+
 class statistics_model
+{
+
+    public  function add( $page_view ){
+        $setting = db('statistics');
+
+        $setting->insert( array(
+            'ip'            => $page_view['ip'],
+            'date'          => time(),
+            'uri_id'        => $page_view['uri_id'],
+            'referer'       => $page_view['referer'],
+            'country'       => $page_view['country'],
+            'region'        => $page_view['region'],
+            'city'          => $page_view['city'],
+            'latitude'      => $page_view['latitude'],
+            'longitude'     => $page_view['longitude'],
+            'browser'       => $page_view['browser'],
+            'browser_full'  => $page_view['browser_full'],
+            'version'       => $page_view['version'],
+            'user_agent'    => $page_view['user_agent'],
+            'os'            => $page_view['os'],
+            'os_version'    => $page_view['os_version']
+        ), FALSE );
+    }
+
+
+    public function add_meta( $key, $value )
+    {
+        $statistics_meta = db('statistics_meta');
+
+        $result = $this->look_up( $value );
+
+        if ( $result == false ):
+
+            $id = $statistics_meta->insert( array(
+                'key'           => $key,
+                'value'         => $value
+            ) );
+            return $id->id;
+        else:
+            return $result;
+        endif;
+    }
+
+
+    public function get_meta( $key )
+    {
+        $setting = db ( 'statistics_meta' );
+
+        $get_settings = $setting->select( '*' )
+            ->where( 'key', '=', $key )
+            ->execute();
+
+        $count = $setting->count()
+            ->where( 'key', '=', $key )
+            ->execute();
+
+        if ( $count == 0 )
+            return false;
+        else
+            return $get_settings[0]->value;
+    }
+
+
+    public function look_up( $value )
+    {
+        $setting = db ( 'statistics_meta' );
+
+        $get_settings = $setting->select( '*' )
+            ->where( 'value', '=', $value )
+            ->execute();
+
+        $count = $setting->count()
+            ->where( 'value', '=', $value )
+            ->execute();
+
+        if ( $count == 0 )
+            return false;
+        else
+            return $get_settings[0]->id;
+    }
+}
+
+class old_statistics_model
 {
     /*
      * Adds a value if it does not exist
      */
-    public function set ( $key, $value )
+    public function set ( $key, $value, $retrun_key = false )
     {
 
         $get =  unserialize( get::option( $key ) );
@@ -33,6 +117,13 @@ class statistics_model
             $get =  unserialize( get::option($key) );
         }
 
+        if ( $retrun_key ){
+            end($get);
+            $retrun_key = key($get);
+
+            return $retrun_key;
+        }
+
         return $get;
     }
 
@@ -46,6 +137,11 @@ class statistics_model
     }
 
 
+    public function page_view()
+    {
+        // save a page view.
+    }
+
     /*
      * Returns the array key from a matching value
      */
@@ -53,10 +149,14 @@ class statistics_model
     {
         $get =  unserialize( get::option ($key ) );
 
-        foreach( $get as $get_key => $get_value )
-        {
-            if ( $get_value == $value )
-                return $get_key;
+        if (is_array($get)){
+            foreach( $get as $get_key => $get_value )
+            {
+                if ( $get_value == $value )
+                    return $get_key;
+            }
         }
+
+        return false;
     }
 }
