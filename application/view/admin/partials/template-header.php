04b6162316248131652e21a9ae59e524543d96fe
Adam Patterson <hello@adampatterson.ca>
2013-02-28 15:56:10 -0700

    #111 includes JS

--- application/view/admin/partials/template-header.php
+++ application/view/admin/partials/template-header.php
@@ -21,10 +21,16 @@
 
 	<link type="text/css" rel="stylesheet" href="<?=ADMIN_CSS; ?>application.css">
 	<link type="text/css" rel="stylesheet" href="<?=ADMIN_CSS; ?>admin.css">
+	<link type="text/css" rel="stylesheet" href="<?=ADMIN_CSS; ?>reveal.css">
 
+    <script type="text/javascript" src="<?=ADMIN_JS; ?>modernizr.min.js"></script>
 	<script type="text/javascript" src="<?=ADMIN_JS; ?>jquery.min.js"></script>
 	<script type="text/javascript" src="<?=ADMIN_JS; ?>jquery-ui.min.js"></script>
-	<script type="text/javascript" src="<?=ADMIN_JS; ?>modernizr.min.js"></script>
+
+    <? if(user_editor() == 'wysiwyg'): ?>
+        <script type="text/javascript" src="<?= ADMIN_JS; ?>raptor.min.js"></script>
+    <? endif; ?>
+
     <script type="text/javascript" src="<?=ADMIN_JS; ?>jquery.reveal.js"></script>
 	<script type="text/javascript" src="<?=ADMIN_JS; ?>jquery.inputtags.js"></script>
 	<script type="text/javascript" src="<?=ADMIN_JS; ?>notifications.js"></script>
@@ -34,13 +40,50 @@
         <script type="text/javascript" src="<?=ADMIN_JS; ?>bootstrap-transition.js"></script>
         <script type="text/javascript" src="<?=ADMIN_JS; ?>bootstrap-collapse.js"></script>
         <script type="text/javascript" src="<?=ADMIN_JS; ?>bootstrap-tab.js"></script>
-    <? else: ?>
-        <script src="<?=ADMIN_JS; ?>bootstrap.min.js"></script>
     <? endif; ?>
 
     <? if(user_editor() == 'wysihtml5'): ?>
+        <script src="<?=ADMIN_JS; ?>bootstrap.min.js"></script>
         <script src="<?=ADMIN_JS; ?>/bootstrap-wysihtml5/dist/bootstrap-wysihtml5.js"></script>
     <? endif; ?>
+
+    <? if ( in_array('flot', $assets)): ?>
+        <script src="<?=ADMIN_JS;?>flot/jquery.flot.js"></script>
+        <script src="<?=ADMIN_JS;?>flot/jquery.flot.pie.js"></script>
+        <script src="<?=ADMIN_JS;?>flot/jquery.flot.stack.js"></script>
+        <script src="<?=ADMIN_JS;?>flot/jquery.flot.resize.js"></script>
+        <script src="<?=ADMIN_JS;?>jquery.sparklines.js"></script>
+        <script type="text/javascript">
+            <?
+            $statistics = load::model('statistics');
+
+            $count = $statistics->count_by_chunks(7);
+            $unique = $statistics->count_by_unique_chunks(7);
+            ?>
+
+
+            var page_views = [ <?
+             foreach ( $count['chunks'] as $key => $value )
+             {
+                echo '['. $key .', '. $value .'],';
+             }
+
+             ?> ];
+
+            var page_view_total = <?= $count['total'] ?>;
+
+            var unique_views = [ <?
+             foreach ( $unique['chunks'] as $key => $value )
+             {
+                echo '['. $key .', '. $value .'],';
+             }
+
+             ?> ];
+
+            var unique_view_total = <?= $unique['total'] ?>;
+         </script>
+
+     <? endif; ?>
 
     <? if ( in_array('sortable', $assets ) ): ?>
         <script type="text/javascript" src="<?=ADMIN_JS; ?>jquery.nestable.js"></script>
