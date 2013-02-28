3a86ad901c6062f7e850595c4f2248fd1e7891ce
Adam Patterson <hello@adampatterson.ca>
2013-02-27 17:14:29 -0700

    JS and Spark line styles

--- tentacle/js/application.js
+++ tentacle/js/application.js
@@ -1,103 +1,106 @@
 $(document).ready(function(){
 	
-	
-    // Minimize Content Box
-	// ====================================
-		$(".content-box-header h3").css({ "cursor":"s-resize" }); // Give the h3 in Content Box Header a different cursor
-		$(".closed-box .content-box-content").hide(); // Hide the content of the header if it has the class "closed"
-		$(".closed-box .content-box-tabs").hide(); // Hide the tabs in the header if it has the class "closed"
-		
-		$(".content-box-header h3").click( // When the h3 is clicked...
-			function () {
-			  $(this).parent().next().toggle(); // Toggle the Content Box
-			  $(this).parent().parent().toggleClass("closed-box"); // Toggle the class "closed-box" on the content box
-			  $(this).parent().find(".content-box-tabs").toggle(); // Toggle the tabs
-			}
-		);
+	function template(){
+        // Minimize Content Box
+        // ====================================
+            $(".content-box-header h3").css({ "cursor":"s-resize" }); // Give the h3 in Content Box Header a different cursor
+            $(".closed-box .content-box-content").hide(); // Hide the content of the header if it has the class "closed"
+            $(".closed-box .content-box-tabs").hide(); // Hide the tabs in the header if it has the class "closed"
 
-	// Admin Tabs:
-	// ====================================
-		$('#content-tabs a').click(function (e) {
-		  e.preventDefault();
-		  $(this).tab('show');
-		})
+            $(".content-box-header h3").click( // When the h3 is clicked...
+                function () {
+                  $(this).parent().next().toggle(); // Toggle the Content Box
+                  $(this).parent().parent().toggleClass("closed-box"); // Toggle the class "closed-box" on the content box
+                  $(this).parent().find(".content-box-tabs").toggle(); // Toggle the tabs
+                }
+            );
 
-    //Close button:
-	// ====================================
-		$(".close").click(
-			function () {
-				$(this).parent().fadeTo(400, 0, function () { // Links with the class "close" will close parent
-					$(this).slideUp(400);
-				});
-				return false;
-			}
-		);
+        // Admin Tabs:
+        // ====================================
+            $('#content-tabs a').click(function (e) {
+              e.preventDefault();
+              $(this).tab('show');
+            })
 
-    // Alternating table rows:
-	// ====================================
-		$('tbody tr:even').addClass("even"); // Add class "alt-row" to even table rows
+        //Close button:
+        // ====================================
+            $(".close").click(
+                function () {
+                    $(this).parent().fadeTo(400, 0, function () { // Links with the class "close" will close parent
+                        $(this).slideUp(400);
+                    });
+                    return false;
+                }
+            );
 
-    // Check all checkboxes when the one in a table head is checked:
-	// ====================================
-		$('.check-all').click(
-			function(){
-				$(this).parent().parent().parent().parent().find("input[type='checkbox']").attr('checked', $(this).is(':checked'));   
-			}
-		);
+        // Alternating table rows:
+        // ====================================
+            $('tbody tr:even').addClass("even"); // Add class "alt-row" to even table rows
 
-	/* Username Lookup */
-	// ====================================
+        // Check all checkboxes when the one in a table head is checked:
+        // ====================================
+            $('.check-all').click(
+                function(){
+                    $(this).parent().parent().parent().parent().find("input[type='checkbox']").attr('checked', $(this).is(':checked'));
+                }
+            );
 
-	
-	/* Tags */
-	// ====================================
-	if ($('body').hasClass('tags')) {
-		$('.tags').tagsInput();
-	}
-	
-	/* Alert */
-	// ====================================
-	if ($('body').hasClass('alert-message')) {
-		$(".alert-message").alert();
-	}
-	
-	/* Modal Window */
-	// ====================================
-	if ($('body').hasClass('alert-message')) {
-		$('#my-modal').modal()
-	}
+        /* Username Lookup */
+        // ====================================
 
 
+        /* Tags */
+        // ====================================
+        if ($('body').hasClass('tags')) {
+            $('.tags').tagsInput();
+        }
 
-	/* Publish on */
-  	// =============================
+        /* Alert */
+        // ====================================
+        if ($('body').hasClass('alert-message')) {
+            $(".alert-message").alert();
+        }
 
-	$(".published-on").hide();
+        /* Modal Window */
+        // ====================================
+        if ($('body').hasClass('alert-message')) {
+            $('#my-modal').modal()
+        }
 
-	$("#publish").change(function(){          
-	    var value = $("#publish option:selected").val();
 
-	    if( value == 'published-on') {
-	        $(".published-on").show();
-	    } else {
-	        $(".published-on").hide();
-	    }
-	});
-	
-	$("a#edit_publish").click(function(){          
-	    var value = $("#publish option:selected").val();
 
-	    if( value == 'published-on') {
-	        $(".published-on").show();
-	    } else {
-	        $(".published-on").hide();
-	    }
-	});
-	
-	$(".published-on").hide();
-	$("#edit_publish").show();
- 
-    $('#edit_publish').click(function(){
-	    $(".published-on").toggle();
-	})
+        /* Publish on */
+        // =============================
+
+        $(".published-on").hide();
+
+        $("#publish").change(function(){
+            var value = $("#publish option:selected").val();
+
+            if( value == 'published-on') {
+                $(".published-on").show();
+            } else {
+                $(".published-on").hide();
+            }
+        });
+
+        $("a#edit_publish").click(function(){
+            var value = $("#publish option:selected").val();
+
+            if( value == 'published-on') {
+                $(".published-on").show();
+            } else {
+                $(".published-on").hide();
+            }
+        });
+
+        $(".published-on").hide();
+        $("#edit_publish").show();
+
+        $('#edit_publish').click(function(){
+            $(".published-on").toggle();
+        })
+    }
+
+    template();
 });
