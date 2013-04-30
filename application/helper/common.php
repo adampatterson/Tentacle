fd23c7f556cc72659b836ca4fc1b1c44c582c989
Adam Patterson <hello@adampatterson.ca>
2013-04-30 13:29:23 -0600

    Closes #156 added a dash to the $strip list.
    This - Title now gets you a URI like this-title.

--- application/helper/common.php
+++ application/helper/common.php
@@ -997,9 +997,9 @@ class string
 	*	Sanitized string, typically for URLs.
 	*/
 	public static function sanitize($string, $force_lowercase = true, $anal = false, $trunc = 100) {
-	    $strip = array("~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "+", "[", "{", "]",
+	    $strip = array("~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]",
 	        "}", "\\", "|", ";", ":", "\"", "'", "&#8216;", "&#8217;", "&#8220;", "&#8221;", "&#8211;", "&#8212;",
-	        "—", "–", ",", "<", ".", ">", "/", "?");
+	        "-", "–", ",", "<", ".", ">", "/", "?");
 
 	    $clean = trim(str_replace($strip, "", strip_tags($string)));
 	    $clean = preg_replace('/\s+/', "-", $clean);
