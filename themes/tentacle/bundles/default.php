6c99078a0e2e1be370f8b8110c378e49c5a1d29b
Adam Patterson <hello@adampatterson.ca>
2013-12-04 23:08:16 -0700

    Tentacle theme cleanup

--- themes/tentacle/bundles/default.php
+++ themes/tentacle/bundles/default.php
@@ -10,8 +10,8 @@ _/      _/_/_/  _/    _/      _/_/    _/_/_/    _/_/_/  _/    _/_/_/
 <? theme::script('http://html5shim.googlecode.com/svn/trunk/html5.js') ?>
 <![endif]-->
 <?
-theme::script('/assets/js/jquery.min.js');
-theme::css('/assets/css/main.css');
-theme::script('/assets/js/application.min.js');
-theme::script('/assets/js/bootstrap.min.js')
+theme::script('assets/js/jquery.min.js');
+theme::css('assets/css/main.css');
+theme::script('assets/js/application.min.js');
+theme::script('assets/js/bootstrap.min.js')
 ?>
