a9b2de052bc3c65e2fb8a5daf8f2d3b2c3ade04b
Adam Patterson <hello@adampatterson.ca>
2014-01-02 01:27:53 -0700

    BS Notify udpate

--- application/view/admin/partials/login-header.php
+++ application/view/admin/partials/login-header.php
@@ -12,8 +12,7 @@
 	_/      _/_/_/  _/    _/      _/_/    _/_/_/    _/_/_/  _/    _/_/_/     
 	======================================================================-->
 	<link type="text/css" rel="stylesheet" href="<?=ADMIN_CSS; ?>admin.css">
-    <script type="text/javascript" src="<?=ADMIN_JS; ?>payload.min.js"></script>
-    <script type="text/javascript" src="<?=ADMIN_JS; ?>bootstrap-notify.js"></script>
+  <script type="text/javascript" src="<?=ADMIN_JS; ?>payload.min.js"></script>
 
 </head>
 <body class="login">
