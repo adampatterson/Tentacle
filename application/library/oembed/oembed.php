0c142fedef59437c18552dc3a1c500062a953d60
Adam Patterson <hello@adampatterson.ca>
2015-03-22 09:50:11 -0600

    Closes #241 inline styles but outputs a frame like http://embedresponsively.com

--- application/library/oembed/oembed.php
+++ application/library/oembed/oembed.php
@@ -56,6 +56,9 @@ function oembed_cotnent( $url, $raw=null )
             else
                 $embed_code =  $oembed_data->html;
 
+            if ( $host == 'www.youtube.com' || $host == 'youtube.com' )
+                $embed_code = '<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class="embed-container">'. $embed_code .'</div>';
+            
             $pattern = "/height=\"[0-9]*\"/";
             $content = preg_replace($pattern, 'height="'.get::option( 'embed_size_h' ).'"', $embed_code);
 
